95a28cc78aa0c3cc47576e4dc61e6c907e48256e
Dominic Sherman <dominic.sherman98@gmail.com>
2021-03-02 09:41:48 -0800

    Add system notification component

---
+++ cardstack/src/components/SystemNotification/SystemNotification.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container, Text } from '@cardstack/components';
+
+export const SystemNotification = () => (
+  <Container>
+    <Text>Prepaid cards are denominated in SPEND</Text>
+  </Container>
+);
