0ccbe894e32abea053e5da66c3c7e5487678877f
Dominic Sherman <dominic.sherman98@gmail.com>
2021-03-20 08:14:21 -0700

    wip

--- src/components/add-cash/AddCashFooter.js
+++ src/components/add-cash/AddCashFooter.js
@@ -4,10 +4,9 @@ import { useTheme } from '../../context/ThemeContext';
 import { useDimensions } from '../../hooks';
 import { useNavigation } from '../../navigation/Navigation';
 import { ButtonPressAnimation } from '../animations';
-import { Icon } from '../icons';
 import { Centered, ColumnWithMargins, Row, RowWithMargins } from '../layout';
-import { Emoji, Text } from '../text';
 import ApplePayButton from './ApplePayButton';
+import { Icon, Text } from '@cardstack/components';
 import Routes from '@rainbow-me/routes';
 import { position } from '@rainbow-me/styles';
 
@@ -44,23 +43,9 @@ const AddCashFooter = ({ disabled, onDisabledPress, onSubmit, ...props }) => {
           scaleTo={0.96}
         >
           <RowWithMargins align="center" margin={3}>
-            <Centered>
-              <Emoji name="earth_americas" size="smedium" />
-            </Centered>
-            <Text
-              color={colors.alpha(colors.blueGreyDark, 0.5)}
-              lineHeight="normal"
-              size="lmedium"
-              weight="semibold"
-            >
-              Works with most debit cards
-            </Text>
+            <Text color="blueText">Works with most debit cards</Text>
             <Centered marginLeft={2} marginTop={0.5}>
-              <Icon
-                {...position.sizeAsObject(18)}
-                color={colors.alpha(colors.blueGreyDark, 0.3)}
-                name="info"
-              />
+              <Icon color="blueText" name="help-circle" />
             </Centered>
           </RowWithMargins>
         </ButtonPressAnimation>
