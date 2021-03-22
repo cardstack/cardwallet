8f298b83ab6d0af5fbd228e799d029d641524207
echo-doug <doug@echobind.com>
2021-03-22 10:26:48 -0700

    Add cov for ganache utils and pray it works

--- cardstack/src/types/react-native-dotenv.d.ts
+++ cardstack/src/types/react-native-dotenv.d.ts
@@ -1,6 +1,5 @@
 declare module 'react-native-dotenv' {
-  export const GANACHE_URL_ANDROID: string;
-  export const GANACHE_URL_IOS: string;
+  export const GANACHE_URL: string;
   export const IMGIX_DOMAIN: string;
   export const IMGIX_TOKEN: string;
 }
--- cardstack/src/utils/ganache-utils.ts
+++ cardstack/src/utils/ganache-utils.ts
@@ -1,6 +1,5 @@
-import { GANACHE_URL_ANDROID, GANACHE_URL_IOS } from 'react-native-dotenv';
+import { GANACHE_URL } from 'react-native-dotenv';
 import { web3SetHttpProvider } from '@rainbow-me/handlers/web3';
-import logger from 'logger';
 import networkTypes from '@rainbow-me/networkTypes';
 
 const TAG = 'GanacheUtils';
@@ -8,15 +7,11 @@ const TAG = 'GanacheUtils';
 export const GanacheUtils = {
   connect: async (callback?: () => void): Promise<void> => {
     try {
-      const network =
-        GANACHE_URL_IOS || GANACHE_URL_ANDROID || 'http://127.0.0.1:7545';
+      const network = GANACHE_URL || 'http://127.0.0.1:7545';
 
-      const ready = await web3SetHttpProvider(network);
-
-      logger.log(TAG, 'connected to ganache', ready);
+      await web3SetHttpProvider(network);
     } catch (e) {
       await web3SetHttpProvider(networkTypes.mainnet);
-      logger.log(TAG, 'error connecting to ganache');
     }
 
     if (callback) {
