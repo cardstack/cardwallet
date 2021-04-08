73add45e5bb347a8070fb5529ba7fcb47830c928
Alex Silcox <alex@echobind.com>
2021-04-08 14:29:12 -0700

    reverting change to biometricbuttoncontent

--- src/components/buttons/BiometricButtonContent.js
+++ src/components/buttons/BiometricButtonContent.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import { Container, Icon, Text } from '@cardstack/components';
+import { Icon } from '../icons';
+import { RowWithMargins } from '../layout';
+import { Text } from '@cardstack/components';
 import BiometryTypes from '@rainbow-me/helpers/biometryTypes';
 import { useBiometryType } from '@rainbow-me/hooks';
+
+const BiometryIcon = styled(Icon).attrs(({ biometryType, color }) => ({
+  color,
+  name: biometryType.toLowerCase(),
+  size: biometryType === BiometryTypes.passcode ? 19 : 20,
+}))`
+  margin-bottom: ${({ biometryType }) =>
+    biometryType === BiometryTypes.passcode ? 1.5 : 0};
+`;
 
 export default function BiometricButtonContent({
   color,
   showIcon,
   text,
   testID,
+  ...props
 }) {
   const biometryType = useBiometryType();
-
   const showBiometryIcon =
     showIcon &&
     (biometryType === BiometryTypes.passcode ||
@@ -23,22 +35,14 @@ export default function BiometricButtonContent({
       biometryType === BiometryTypes.Face);
 
   return (
-    <Container centered margin={7}>
+    <RowWithMargins centered margin={7} {...props}>
       {!android && showBiometryIcon && (
-        <Icon
-          color={color}
-          name={
-            showBiometryIcon
-              ? 'thumbprint'
-              : showFaceIDCharacter
-              ? 'face-id'
-              : null
-          }
-        />
+        <BiometryIcon biometryType={biometryType} color={color} />
       )}
       <Text color="settingsGray" fontWeight="600" testID={testID}>
+        {showFaceIDCharacter && '􀎽 '}
         {text}
       </Text>
-    </Container>
+    </RowWithMargins>
   );
 }
