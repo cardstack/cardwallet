8ea6aeff5dabc077f4ce55db3400fe3fec85f5ce
Alex Silcox <alex@echobind.com>
2021-04-16 20:20:44 -0700

    storybook data

--- cardstack/src/components/RadioList/RadioList.story.tsx
+++ cardstack/src/components/RadioList/RadioList.story.tsx
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { storiesOf } from '@storybook/react-native';
+import { current } from 'immer';
+import lodash from 'lodash';
 import React from 'react';
 import { Alert } from 'react-native';
 
 import { Container } from '../Container';
-import { RadioList } from './RadioList';
+import { RadioItemProps, RadioList } from './RadioList';
 
 const networks = [
   {
@@ -26,27 +28,50 @@ const networks = [
     name: 'Sokol',
     value: 'sokol',
   },
+  {
+    color: '#3cc29e',
+    disabled: false,
+    exchange_enabled: true,
+    faucet_url: null,
+    layer: 2,
+    name: 'Sokol',
+    value: 'sokol',
+  },
+  {
+    color: '#3cc29e',
+    disabled: false,
+    exchange_enabled: true,
+    faucet_url: null,
+    layer: 2,
+    name: 'Sokol',
+    value: 'sokol',
+  },
 ];
 
-const DATA = networks
-  .filter(({ disabled }) => !disabled)
-  .map(({ disabled, name, layer }, index) => ({
-    title: layer,
-    data: [
+const groupByLayer = networks
+  .reduce((result: any, curr: any, currentIndex: number) => {
+    result[curr.layer] =
       {
-        disabled,
-        key: index,
-        label: name,
-        value: name,
-      },
-    ],
-  }));
+        title: curr.layer,
+        data: [
+          ...(result[curr.layer]?.data || []),
+          {
+            disabled: curr.disabled,
+            key: currentIndex,
+            label: curr.name,
+            value: curr.name,
+          },
+        ],
+      } || {};
 
-console.log({ DATA });
+    return result;
+  }, [])
+  .flat();
+
 storiesOf('Radio List', module).add('Default', () => (
   <Container backgroundColor="white" width="100%" padding={4}>
     <RadioList
-      items={DATA}
+      items={groupByLayer}
       onChange={(value: string) => Alert.alert('pressed', value)}
       defaultValue={0}
     />
