e281f52fc1240aac85b262da7dcfb4011c8b33cf
Dominic Sherman <dominic.sherman98@gmail.com>
2021-04-19 14:12:19 -0700

    Filter safes by prepaid cards

--- cardstack/src/services/gnosis-service.ts
+++ cardstack/src/services/gnosis-service.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
+import Web3 from 'web3';
+import prepaidCardManagerContract from '../../../src/references/prepaid-card-manager-contract';
 import logger from 'logger';
+import { web3Provider } from '@rainbow-me/handlers/web3';
 
 const baseUrl = 'https://safe-transaction.xdai.gnosis.io/api/v1';
 
@@ -24,7 +27,20 @@ const fetchOwnersByAddress = async (address: string) => {
 };
 
 export const usePrepaidCards = (address: string) => {
+  const web3 = new Web3();
+
+  const contract = new web3.eth.Contract(
+    prepaidCardManagerContract as any,
+    '0xeBDb1731dA9a5FC972DeD53E34AB2daC3B2565F7'
+  );
+
   const { data } = useQuery('owners', () => fetchOwnersByAddress(address));
 
-  return data?.safes || [];
+  return (
+    data?.safes.filter(async (safeAddress: string) => {
+      const cardDetail = await contract.methods.cardDetails(safeAddress).call();
+
+      return cardDetail;
+    }) || []
+  );
 };
