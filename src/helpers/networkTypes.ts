4034827bcdf323f66f2522c17d35c21feb1757ce
Dominic Sherman <dominic.sherman98@gmail.com>
2021-05-13 06:14:55 -0700

    Remove ropsten, rinkeby, and goerli networks

--- src/helpers/networkTypes.ts
+++ src/helpers/networkTypes.ts
@@ -1,22 +1,16 @@
 export enum Network {
-  goerli = 'goerli',
   kovan = 'kovan',
   mainnet = 'mainnet',
-  rinkeby = 'rinkeby',
   sokol = 'sokol',
-  ropsten = 'ropsten',
   xdai = 'xdai',
 }
 
 // We need to keep this one until
 // we have typescript everywhere
 export const networkTypes = {
-  goerli: 'goerli' as Network,
   kovan: 'kovan' as Network,
   mainnet: 'mainnet' as Network,
-  rinkeby: 'rinkeby' as Network,
   sokol: 'sokol' as Network,
-  ropsten: 'ropsten' as Network,
   xdai: 'xdai' as Network,
 };
 
