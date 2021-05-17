a531b57c5e06ef6b43bbe6ec25db35d2b6913847
Dominic Sherman <dominic.sherman98@gmail.com>
2021-05-17 09:51:37 -0700

    Import store correctly

--- src/utils/isETH.ts
+++ src/utils/isETH.ts
@@ -1,6 +1,6 @@
 import { getConstantByNetwork } from '@cardstack/cardpay-sdk';
 import { toLower } from 'lodash';
-import { store } from '@rainbow-me/redux/store';
+import store from '@rainbow-me/redux/store';
 
 export default function isETH(address: string): boolean {
   const network = store.getState().settings.network;
