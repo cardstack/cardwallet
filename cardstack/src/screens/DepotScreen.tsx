61b721da5bdabd9d1991726e37b8678a1f14e951
Dominic Sherman <dominic.sherman98@gmail.com>
2021-05-26 06:44:58 -0700

    Add info and more icons

--- cardstack/src/screens/DepotScreen.tsx
+++ cardstack/src/screens/DepotScreen.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { Linking, StatusBar } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import { getConstantByNetwork } from '@cardstack/cardpay-sdk';
 import { BackButton } from '../../../src/components/header';
 import isNativeStackAvailable from '../../../src/helpers/isNativeStackAvailable';
-import { CenteredContainer, Container, Text } from '@cardstack/components';
+import {
+  CenteredContainer,
+  Container,
+  Icon,
+  Text,
+  Touchable,
+} from '@cardstack/components';
 import { DepotType } from '@cardstack/types';
 import { getAddressPreview } from '@cardstack/utils';
 import { useNavigation } from '@rainbow-me/navigation';
+import { showActionSheetWithOptions } from '@rainbow-me/utils';
+import { useRainbowSelector } from '@rainbow-me/redux/hooks';
 
 interface RouteType {
   params: { depot: DepotType };
@@ -16,12 +25,30 @@ interface RouteType {
 
 export default function DepotScreen() {
   const { goBack } = useNavigation();
+  const network = useRainbowSelector(state => state.settings.network);
+  const blockExplorer = getConstantByNetwork('blockExplorer', network);
 
   const {
     params: { depot },
   } = useRoute<RouteType>();
 
   const { address } = depot;
+
+  console.log({ address });
+
+  const onPressInformation = () => {
+    showActionSheetWithOptions(
+      {
+        options: ['View on Blockscout', 'Cancel'],
+        cancelButtonIndex: 1,
+      },
+      (buttonIndex: number) => {
+        if (buttonIndex === 0) {
+          Linking.openURL(`${blockExplorer}/address/${address}`);
+        }
+      }
+    );
+  };
 
   return (
     <Container backgroundColor="blue" top={0} width="100%">
@@ -43,9 +70,24 @@ export default function DepotScreen() {
                 <Text color="white" weight="bold">
                   Depot
                 </Text>
-                <Text fontFamily="RobotoMono-Regular" color="white" size="xs">
-                  {getAddressPreview(address)}
-                </Text>
+                <Touchable onPress={onPressInformation}>
+                  <Container flexDirection="row" alignItems="center">
+                    <Text
+                      fontFamily="RobotoMono-Regular"
+                      color="white"
+                      size="xs"
+                      marginRight={2}
+                    >
+                      {getAddressPreview(address)}
+                    </Text>
+                    <Icon name="info" size={15} />
+                  </Container>
+                </Touchable>
+              </Container>
+              <Container right={20} position="absolute">
+                <Touchable onPress={onPressInformation}>
+                  <Icon name="more-horizontal" color="white" />
+                </Touchable>
               </Container>
             </CenteredContainer>
           </Container>
