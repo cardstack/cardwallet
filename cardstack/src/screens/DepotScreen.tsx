97ae55d5dcade754d0ba516cc5d9ba20d358a29b
Dominic Sherman <dominic.sherman98@gmail.com>
2021-05-26 11:38:20 -0700

    Add tab view to depot screen

--- cardstack/src/screens/DepotScreen.tsx
+++ cardstack/src/screens/DepotScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Linking, StatusBar } from 'react-native';
+import React, { useState } from 'react';
+import { Linking, StatusBar, LayoutAnimation } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { getConstantByNetwork } from '@cardstack/cardpay-sdk';
 import { BackButton } from '../../../src/components/header';
@@ -23,10 +23,16 @@ interface RouteType {
   name: string;
 }
 
+enum Tabs {
+  BALANCES = 'Balances',
+  ACTIVITIES = 'Activities',
+}
+
 export default function DepotScreen() {
   const { goBack } = useNavigation();
   const network = useRainbowSelector(state => state.settings.network);
   const blockExplorer = getConstantByNetwork('blockExplorer', network);
+  const [selectedTab, setSelectedTab] = useState(Tabs.BALANCES);
 
   const {
     params: { depot },
@@ -49,16 +55,16 @@ export default function DepotScreen() {
   };
 
   return (
-    <Container backgroundColor="blue" top={0} width="100%">
+    <Container top={0} width="100%">
       <StatusBar barStyle="light-content" />
       <Container height="100%" justifyContent="flex-end" paddingBottom={4}>
-        <Container paddingTop={14} paddingBottom={4} backgroundColor="black">
+        <Container paddingTop={14} backgroundColor="black">
           <Container paddingTop={isNativeStackAvailable ? 4 : 1}>
             <CenteredContainer flexDirection="row">
               <Container left={0} position="absolute">
                 {/* @ts-ignore it thinks the JS file requires other props */}
                 <BackButton
-                  color="white"
+                  color="blue"
                   direction="left"
                   onPress={goBack}
                   testID="goToBalancesFromScanner"
@@ -84,17 +90,73 @@ export default function DepotScreen() {
               </Container>
               <Container right={20} position="absolute">
                 <Touchable onPress={onPressInformation}>
-                  <Icon name="more-horizontal" color="white" />
+                  <Icon name="more-horizontal" color="blue" />
                 </Touchable>
               </Container>
             </CenteredContainer>
           </Container>
+          <Container
+            flexDirection="row"
+            style={{ paddingHorizontal: '10%' }}
+            justifyContent="space-between"
+          >
+            <TabHeader
+              tab={Tabs.BALANCES}
+              selectedTab={selectedTab}
+              setSelectedTab={setSelectedTab}
+            />
+            <TabHeader
+              tab={Tabs.ACTIVITIES}
+              selectedTab={selectedTab}
+              setSelectedTab={setSelectedTab}
+            />
+          </Container>
         </Container>
 
         <CenteredContainer flex={1} width="100%">
-          <Text>Depot stuffs</Text>
+          {selectedTab === Tabs.BALANCES ? <Balances /> : <Activities />}
         </CenteredContainer>
       </Container>
     </Container>
   );
 }
+
+interface TabHeaderProps {
+  setSelectedTab: (tab: Tabs) => void;
+  selectedTab: Tabs;
+  tab: Tabs;
+}
+
+const TabHeader = ({ tab, selectedTab, setSelectedTab }: TabHeaderProps) => {
+  const isSelected = selectedTab === tab;
+  return (
+    <Touchable
+      alignItems="center"
+      justifyContent="center"
+      width="40%"
+      paddingTop={5}
+      onPress={() => setSelectedTab(tab)}
+    >
+      <Text
+        color="white"
+        marginBottom={3}
+        weight={isSelected ? 'bold' : 'regular'}
+      >
+        {tab}
+      </Text>
+      <Container
+        backgroundColor={isSelected ? 'white' : 'black'}
+        height={4}
+        width="100%"
+      />
+    </Touchable>
+  );
+};
+
+const Balances = () => {
+  return <Text>Balances</Text>;
+};
+
+const Activities = () => {
+  return <Text>Activities</Text>;
+};
