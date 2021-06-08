b8f2f8f768802d5a39a06f3348210a7a9bee16bf
Dominic Sherman <dominic.sherman98@gmail.com>
2021-06-08 09:00:13 -0700

    Add tab view

--- cardstack/src/components/HorizontalDivider/HorizontalDivider.tsx
+++ cardstack/src/components/HorizontalDivider/HorizontalDivider.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { ContainerProps } from '../Container';
 import { Container } from '@cardstack/components';
 
-export const HorizontalDivider = () => (
+export const HorizontalDivider = (props: ContainerProps) => (
   <Container
     marginVertical={4}
     height={1}
     backgroundColor="borderGray"
     width="100%"
+    {...props}
   />
 );
