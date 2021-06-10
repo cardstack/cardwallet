eeb2503f718dc30246d0cf5488ff738dab908f7f
Dominic Sherman <dominic.sherman98@gmail.com>
2021-06-10 09:10:52 -0700

    Handle copying animations and backup manually modal (#161)

--- src/components/backup/BackupManualStep.js
+++ src/components/backup/BackupManualStep.js
@@ -1,21 +1,19 @@
 import { useRoute } from '@react-navigation/native';
 import analytics from '@segment/analytics-react-native';
 import React, { Fragment, useCallback, useEffect, useState } from 'react';
-import { View } from 'react-native';
 import styled from 'styled-components';
+
 import { useTheme } from '../../context/ThemeContext';
 import { Column } from '../layout';
 import { SecretDisplaySection } from '../secret-display';
-import { SheetActionButton } from '../sheet';
-import { Nbsp, Text } from '../text';
-import WalletTypes from '@rainbow-me/helpers/walletTypes';
+import { Button, Container, Icon, Text } from '@cardstack/components';
+import walletTypes from '@rainbow-me/helpers/walletTypes';
 import {
   useDimensions,
   useWalletManualBackup,
   useWallets,
 } from '@rainbow-me/hooks';
 import { useNavigation } from '@rainbow-me/navigation';
-import { padding } from '@rainbow-me/styles';
 
 const Content = styled(Column).attrs({
   align: 'center',
@@ -26,45 +24,6 @@ const Content = styled(Column).attrs({
   padding-top: ${({ isTallPhone }) => (android ? 30 : isTallPhone ? 65 : 15)};
 `;
 
-const Footer = styled(Column).attrs({
-  align: 'center',
-  justify: 'center',
-})`
-  ${padding(0, 15, 21)};
-  width: 100%;
-`;
-
-const Masthead = styled(Column).attrs({
-  align: 'center',
-  justify: 'start',
-})`
-  padding-top: 18;
-`;
-
-const MastheadDescription = styled(Text).attrs(({ theme: { colors } }) => ({
-  align: 'center',
-  color: colors.alpha(colors.blueGreyDark, 0.6),
-  lineHeight: 'looser',
-  size: 'large',
-}))`
-  max-width: 291;
-`;
-
-const MastheadIcon = styled(Text).attrs({
-  align: 'center',
-  color: 'appleBlue',
-  size: 43,
-  weight: 'semibold',
-})``;
-
-const MastheadTitle = styled(Text).attrs({
-  align: 'center',
-  size: 'big',
-  weight: 'bold',
-})`
-  ${padding(15, 0, 12)};
-`;
-
 export default function BackupManualStep() {
   const { isTallPhone } = useDimensions();
   const { goBack } = useNavigation();
@@ -72,7 +31,6 @@ export default function BackupManualStep() {
   const { onManuallyBackupWalletId } = useWalletManualBackup();
   const { params } = useRoute();
   const walletId = params?.walletId || selectedWallet.id;
-  const { colors } = useTheme();
 
   const [type, setType] = useState(null);
   const [secretLoaded, setSecretLoaded] = useState(false);
@@ -93,45 +51,36 @@ export default function BackupManualStep() {
     });
   }, []);
 
+  const isPrivateKey = type === walletTypes.privateKey;
+  const subText = isPrivateKey
+    ? 'This is the key to your wallet!. Copy it and save it in your password manager, or in another secure spot.'
+    : 'These words are the keys to your wallet! Write them down or save them in your password manager.';
+  const buttonText = `I've saved ${isPrivateKey ? 'my key' : 'these words'}`;
+
   return (
     <Fragment>
-      <Masthead>
-        <MastheadIcon>􀉆</MastheadIcon>
-        <MastheadTitle>Back up manually</MastheadTitle>
-        <MastheadDescription>
-          <MastheadDescription weight="semibold">
-            {type === WalletTypes.privateKey
-              ? `This is the key to your wallet!`
-              : `These words are the keys to your wallet!`}
-          </MastheadDescription>
-          <Nbsp />
-          {type === WalletTypes.privateKey
-            ? `Copy it and save it in your password manager, or in another secure spot.`
-            : `Write them down or save them in your password manager.`}
-        </MastheadDescription>
-      </Masthead>
-      <Content isTallPhone={isTallPhone} paddingHorizontal={30}>
+      <Container alignItems="center" marginTop={5} paddingHorizontal={10}>
+        <Icon color="settingsGray" name="file-text" size={35} />
+        <Text marginTop={2} size="medium" weight="bold">
+          Manual Backup
+        </Text>
+        <Text color="blueText" marginTop={2} textAlign="center">
+          {subText}
+        </Text>
+      </Container>
+      <Content isTallPhone={isTallPhone}>
         <SecretDisplaySection
           onSecretLoaded={setSecretLoaded}
           onWalletTypeIdentified={setType}
         />
       </Content>
-      <Footer>
-        {secretLoaded && (
-          <View marginTop={30}>
-            <SheetActionButton
-              color={colors.appleBlue}
-              fullWidth
-              label={`􀁣 I’ve saved ${
-                type === WalletTypes.privateKey ? 'my key' : 'these words'
-              }`}
-              onPress={onComplete}
-              size="big"
-              weight="bold"
-            />
-          </View>
-        )}
-      </Footer>
+      {secretLoaded && (
+        <Container paddingHorizontal={5} width="100%">
+          <Button marginTop={3} onPress={onComplete} width="100%">
+            {buttonText}
+          </Button>
+        </Container>
+      )}
     </Fragment>
   );
 }
