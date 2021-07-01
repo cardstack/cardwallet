e96f927d09e98dcbe6259414eee2a0454b264713
Dominic Sherman <dominic.sherman98@gmail.com>
2021-07-01 13:11:46 -0500

    Fix TS

--- cardstack/test/test-utils/model-factory.ts
+++ cardstack/test/test-utils/model-factory.ts
@@ -8,23 +8,17 @@ export const createRandomTransactionItem = (
   item: Partial<TransactionItemType> = {}
 ): TransactionItemType => ({
   from: chance.guid(),
-  nonce: chance.natural(),
   to: chance.guid(),
   balance: {
     amount: chance.natural().toString(),
     display: chance.string(),
   },
-  description: chance.string(),
-  gasLimit: chance.string(),
-  gasPrice: chance.string(),
   hash: chance.string(),
   minedAt: chance.natural(),
-  name: chance.string(),
   native: {
     amount: chance.natural().toString(),
     display: chance.string(),
   },
-  pending: chance.bool(),
   status: TransactionStatus.sent,
   symbol: 'ETH',
   title: 'Sent',
