d0d60aec4d5b024b0b13a92b85ff0e0122dfde81
Dominic Sherman <dominic.sherman98@gmail.com>
2021-07-15 10:10:58 -0700

    ts

--- cardstack/src/components/Transactions/TransactionCoinRow.story.tsx
+++ cardstack/src/components/Transactions/TransactionCoinRow.story.tsx
@@ -11,24 +11,18 @@ storiesOf('Transaction Coin Row', module).add('Default', () => {
       <TransactionCoinRow
         item={{
           from: '0xAa262652e7459693fdA194b33d288b487908E520',
-          nonce: 0,
           to: '0xf7FBF7200F2D98979858127baF22FB85c94f3592',
           balance: {
             amount: '5',
             display: '5.00 ETH',
           },
-          description: 'Ethereum',
-          gasLimit: '21000',
-          gasPrice: '142029000000',
           hash:
             '0x57d91b5b7e5259c009e21f8d8e2ea06e7ea437ff4a2e5391fdc47de573cef3fd-0',
           minedAt: 1615326853,
-          name: 'Ethereum',
           native: {
             amount: '5',
             display: '$5.00',
           },
-          pending: false,
           status: TransactionStatus.sent,
           symbol: 'ETH',
           title: 'Sent',
