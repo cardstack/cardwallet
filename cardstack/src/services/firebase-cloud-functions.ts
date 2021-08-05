7139eedff775c9cbc5e5350c082cb42e0d9537f5
danibonilha <dani.bonilha@live.com>
2021-08-05 16:22:50 -0300

    [CS-1514]: Add fb functions modules

---
+++ cardstack/src/services/firebase-cloud-functions.ts
@@ -0,0 +1,31 @@
+import logger from 'logger';
+
+enum CloudFuncNames {
+  MINIMUM_VERSION = 'minimumVersion',
+  MAINTENANCE_STATUS = 'maintenanceStatus',
+  REVIEW_FEATURE = 'reviewFeature',
+}
+
+export const baseCloudFunctionsUrl =
+  'https://us-central1-card-pay-3e9be.cloudfunctions.net';
+
+const createCloudFuncGetRequest = async (name: CloudFuncNames) => {
+  try {
+    const response = await fetch(`${baseCloudFunctionsUrl}/${name}`);
+
+    return await response.json();
+  } catch (e) {
+    logger.error(`get${name}`, e);
+
+    return false;
+  }
+};
+
+export const getMaintenanceStatus = () =>
+  createCloudFuncGetRequest(CloudFuncNames.MAINTENANCE_STATUS);
+
+export const getMinimumVersion = () =>
+  createCloudFuncGetRequest(CloudFuncNames.MINIMUM_VERSION);
+
+export const getReviewFeature = () =>
+  createCloudFuncGetRequest(CloudFuncNames.REVIEW_FEATURE);
