09ab00a7d585af498fa25cb21abcb9e5365d9b10
Brian <brianwebtek@gmail.com>
2021-08-13 03:40:42 +0800

    Cleanup non-using transaction service

--- cardstack/src/services/index.ts
+++ cardstack/src/services/index.ts
@@ -2,4 +2,3 @@ export * from './gnosis-service';
 export * from './historical-pricing-service';
 export * from './exchange-rate-service';
 export * from './firebase-cloud-functions';
-export * from './transaction-mapping-service';
