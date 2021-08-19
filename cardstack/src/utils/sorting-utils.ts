b8c03af6694e8f0594d5198d03c29633de088c92
Eduardo Shinkawa <eshinkawa@gmail.com>
2021-08-19 16:47:11 -0300

    [CS-1661] Order largest value first in merchant available balances and unclaimed revenue

--- cardstack/src/utils/sorting-utils.ts
+++ cardstack/src/utils/sorting-utils.ts
@@ -1,6 +1,13 @@
+import { TokenType } from '@cardstack/types';
+
 export const sortByTime = (a: any, b: any) => {
   const timeA = Number(a.timestamp || a.minedAt || a.createdAt);
   const timeB = Number(b.timestamp || b.minedAt || b.createdAt);
 
   return timeB - timeA;
 };
+
+export const sortedByTokenBalanceAmount = (tokenItems: any): TokenType[] =>
+  [...tokenItems].sort((a: any, b: any) => {
+    return b.native?.balance?.amount - a.native?.balance?.amount;
+  }) || [];
