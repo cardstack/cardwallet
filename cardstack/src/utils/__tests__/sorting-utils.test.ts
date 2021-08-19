4d2abbd9e5cf477f09f3dcebbd8673b684c8e303
Eduardo Shinkawa <eshinkawa@gmail.com>
2021-08-19 18:00:22 -0300

    [CS-1661] Order largest value first in merchant available balances and unclaimed revenue (#326)

---
+++ cardstack/src/utils/__tests__/sorting-utils.test.ts
@@ -0,0 +1,54 @@
+import { sortedByTokenBalanceAmount } from '../sorting-utils';
+
+describe('Sorting by token balance amount', () => {
+  it('should return the same when list has only one item or empty', () => {
+    const mockData = [
+      {
+        tokenAddress: '0xFeDc0c803390bbdA5C4C296776f4b574eC4F30D1',
+        tokenSymbol: 'DAI',
+        token: { symbol: 'DAI' },
+        balance: { amount: '0.000173983', display: '0.000174 DAI' },
+        native: { balance: { amount: 0.00017433, display: '$0.000174 USD' } },
+      },
+    ];
+
+    expect(sortedByTokenBalanceAmount(mockData)).toStrictEqual(mockData);
+    expect(sortedByTokenBalanceAmount([])).toStrictEqual([]);
+  });
+
+  it('should return ordered list with the one with biggest amount on the top', () => {
+    const mockData = [
+      {
+        tokenAddress: '0xB236ca8DbAB0644ffCD32518eBF4924ba866f7Ee',
+        token: {
+          name: 'CARD Token Kovan.CPXD',
+          symbol: 'CARD',
+          decimals: 18,
+          value: '100',
+        },
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        coingecko_id: 'cardstack',
+        balance: { amount: '100', display: '100.00 CARD' },
+        native: { balance: { amount: 0.733227, display: '$0.733 USD' } },
+      },
+      {
+        tokenAddress: '0xFeDc0c803390bbdA5C4C296776f4b574eC4F30D1',
+        token: {
+          name: 'Dai Stablecoin.CPXD',
+          symbol: 'DAI',
+          decimals: 18,
+          value: '3.475236916195608782',
+        },
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        coingecko_id: 'dai',
+        balance: { amount: '3.475236916195608782', display: '3.475 DAI' },
+        native: { balance: { amount: 3.48218739, display: '$3.48 USD' } },
+      },
+    ];
+
+    expect(sortedByTokenBalanceAmount(mockData)[0]).toHaveProperty(
+      'tokenAddress',
+      '0xFeDc0c803390bbdA5C4C296776f4b574eC4F30D1'
+    );
+  });
+});
