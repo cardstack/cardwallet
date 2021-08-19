498282c9d923420c00cf59505e91c459ea2daeb7
Dominic Sherman <dominic.sherman98@gmail.com>
2021-08-19 07:13:13 -0700

    Optional chain

--- cardstack/src/hooks/transactions/use-transaction-sections.tsx
+++ cardstack/src/hooks/transactions/use-transaction-sections.tsx
@@ -64,7 +64,7 @@ export const useTransactionSections = ({
             nativeCurrency,
             currencyConversionRates,
             transactionStrategies,
-            depotAddress: depot.address,
+            depotAddress: depot?.address,
             merchantSafeAddress,
           });
 
