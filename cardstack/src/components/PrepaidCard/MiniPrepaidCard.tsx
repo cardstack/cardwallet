5c5ffe3017038a53ecec2e3cf48ad3904ea144fa
danibonilha <dani.bonilha@live.com>
2021-08-31 19:09:20 -0300

    [CS-1498] - Create MiniPrepaidCard

---
+++ cardstack/src/components/PrepaidCard/MiniPrepaidCard.tsx
@@ -0,0 +1,43 @@
+import React, { memo } from 'react';
+import { StyleSheet } from 'react-native';
+import { Container } from '@cardstack/components';
+import { CustomizableBackground } from '@cardstack/components/PrepaidCard/CustomizableBackground';
+import { PrepaidCardCustomization } from '@cardstack/types';
+
+const styles = StyleSheet.create({
+  cardWrapper: {
+    shadowColor: 'black',
+    shadowOffset: {
+      height: 3,
+      width: 0,
+    },
+    shadowRadius: 3,
+    shadowOpacity: 0.3,
+    borderRadius: 3,
+    borderWidth: 0.3,
+    overflow: 'hidden',
+  },
+});
+
+const MiniPrepaidCard = ({
+  cardCustomization,
+}: {
+  cardCustomization: PrepaidCardCustomization;
+}) => (
+  <Container
+    width={20}
+    height={16}
+    borderColor="borderGray"
+    style={styles.cardWrapper}
+  >
+    <CustomizableBackground
+      cardCustomization={{ ...cardCustomization, patternUrl: null }} // Not applying pattern on miniCard
+      isEditing={false}
+      variant="mini"
+      address="miniPrepaidCard"
+    />
+    <Container />
+  </Container>
+);
+
+export default memo(MiniPrepaidCard);
