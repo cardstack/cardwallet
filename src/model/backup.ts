8f152aa36c6a1e6705b8215e170399e4c31a87a0
danibonilha <dani.bonilha@live.com>
2021-09-01 18:15:42 -0300

    [CS-1704] - Add universal link and remove rainbow

--- src/model/backup.ts
+++ src/model/backup.ts
@@ -222,7 +222,11 @@ export async function saveBackupPassword(
 ): Promise<void> {
   try {
     if (ios) {
-      await setSharedWebCredentials('rainbow.me', 'Backup Password', password);
+      await setSharedWebCredentials(
+        'cardstack.cardpay',
+        'Backup Password',
+        password
+      );
     }
   } catch (e) {
     logger.sentry('Error while backing up password');
