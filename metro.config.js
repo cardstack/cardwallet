48b8282fde33b01d76df6d6636b9befd29a5ab51
Brian <brianwebtek@gmail.com>
2021-09-04 01:59:55 +0800

    Remove react-native-animated-charts

--- metro.config.js
+++ metro.config.js
@@ -4,10 +4,6 @@ const blacklist = require('metro-config/src/defaults/blacklist');
 // Deny list is a function that takes an array of regexes and combines
 // them with the default blacklist to return a single regex.
 const blacklistRE = blacklist([
-  // react-native-animated-charts
-  /src\/react-native-animated-charts\/Example\/.*/,
-  /src\/react-native-animated-charts\/node_modules\/.*/,
-  'src.react-native-animated-charts.package.json',
   // react-native-reanimated <patch>
   /patches\/reanimated\/.*/,
 ]);
