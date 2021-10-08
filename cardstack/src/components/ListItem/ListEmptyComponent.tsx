6942ce1983804ccf993cb763c6f20b1b2677cfd3
Brian <brianwebtek@gmail.com>
2021-10-08 17:27:10 +0800

    Update list empty component for all transactions list, asset list

--- cardstack/src/components/ListItem/ListEmptyComponent.tsx
+++ cardstack/src/components/ListItem/ListEmptyComponent.tsx
@@ -6,16 +6,25 @@ import { ColorTypes } from '@cardstack/theme';
 interface ListEmptyComponentProps extends ContainerProps {
   text?: string;
   textColor?: ColorTypes;
+  hasRoundBox?: boolean;
 }
 
 export const ListEmptyComponent = ({
   text = 'Empty List',
   textColor = 'grayText',
+  hasRoundBox,
   ...props
 }: ListEmptyComponentProps) => {
   return (
-    <CenteredContainer flex={1} height={100} width="100%" {...props}>
-      <Text color={textColor} textAlign="center">
+    <CenteredContainer
+      flex={1}
+      borderRadius={10}
+      backgroundColor={hasRoundBox ? 'buttonDisabledBackground' : 'transparent'}
+      paddingVertical={9}
+      marginHorizontal={5}
+      {...props}
+    >
+      <Text color={textColor} textAlign="center" size="body">
         {text}
       </Text>
     </CenteredContainer>
