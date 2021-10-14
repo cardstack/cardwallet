3b1cac585c2f070f9f06a367108bc8ab54122c56
Dani Bonilha <dani.bonilha@live.com>
2021-10-14 18:20:03 -0300

    [Chore]:  Create reduxLogger middleware (#417)

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -4,15 +4,42 @@ import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 const enableReduxFlipper = false;
+const enableReduxLogger = false;
 
-const middlewares = [thunk];
+// Ignoring types, bc it's just a debug helper
+const reduxLogger = (store: any) => (next: any) => (action: any) => {
+  const reducersKeys = Object.keys(reducers);
 
-if (__DEV__ && enableReduxFlipper) {
-  const createDebugger = require('redux-flipper').default;
-  middlewares.push(createDebugger({ resolveCyclic: true }));
-}
+  // We can specify which keys to log  or log all
+  const singleKeys: string[] = ['wallets', 'appState'];
+  const stateKeys = singleKeys.length ? singleKeys : reducersKeys;
 
-const store = createStore(reducers, applyMiddleware(...middlewares));
+  console.info('dispatching', JSON.stringify(action));
+  let result = next(action);
+
+  stateKeys.forEach(key => {
+    console.log(`${key} state`, JSON.stringify(store.getState()?.[key]));
+  });
+
+  return result;
+};
+
+const configureStore = () => {
+  const middlewares = [thunk];
+
+  if (__DEV__) {
+    enableReduxLogger && middlewares.push(reduxLogger as any);
+
+    if (enableReduxFlipper) {
+      const createDebugger = require('redux-flipper').default;
+      middlewares.push(createDebugger({ resolveCyclic: true }));
+    }
+  }
+
+  return createStore(reducers, applyMiddleware(...middlewares));
+};
+
+const store = configureStore();
 
 export default store;
 
