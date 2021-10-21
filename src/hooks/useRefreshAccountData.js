2ea137f1b101190818bc5cb597acdea5736a9519
danibonilha <dani.bonilha@live.com>
2021-10-21 17:10:54 -0300

    [CS-2220]: Fix app refresh to wait updated assets

--- src/hooks/useRefreshAccountData.js
+++ src/hooks/useRefreshAccountData.js
@@ -1,13 +1,9 @@
 import { captureException } from '@sentry/react-native';
-import delay from 'delay';
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { uniqueTokensRefreshState } from '../redux/uniqueTokens';
 import { fetchWalletNames } from '../redux/wallets';
-import {
-  fallbackExplorerClearState,
-  fallbackExplorerInit,
-} from '@rainbow-me/redux/fallbackExplorer';
+import { fetchAssetsBalancesAndPrices } from '@rainbow-me/redux/fallbackExplorer';
 import logger from 'logger';
 
 export default function useRefreshAccountData() {
@@ -15,18 +11,13 @@ export default function useRefreshAccountData() {
 
   const refreshAccountData = useCallback(async () => {
     try {
-      await dispatch(fallbackExplorerClearState());
       const getWalletNames = dispatch(fetchWalletNames());
       const getUniqueTokens = dispatch(uniqueTokensRefreshState());
-      const explorer = dispatch(fallbackExplorerInit());
 
       return Promise.all([
-        explorer,
         getWalletNames,
         getUniqueTokens,
-        // We will remove this and track the loading based on fallbackExplorer
-        // This is a workaround to have the animation while updating the assets
-        delay(12000), // minimum duration we want the "Pull to Refresh" animation to last
+        fetchAssetsBalancesAndPrices(),
       ]);
     } catch (error) {
       logger.log('Error refreshing data', error);
