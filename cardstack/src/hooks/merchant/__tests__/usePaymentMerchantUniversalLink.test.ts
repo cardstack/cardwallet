85a47d7b81de2c367a9f50ce44f83f3550ff806d
danibonilha <dani.bonilha@live.com>
2021-10-22 12:39:06 -0300

    [CS-2188]: Add from param to get right acc address

--- cardstack/src/hooks/merchant/__tests__/usePaymentMerchantUniversalLink.test.ts
+++ cardstack/src/hooks/merchant/__tests__/usePaymentMerchantUniversalLink.test.ts
@@ -22,6 +22,7 @@ jest.mock('@react-navigation/native', () => ({
 jest.mock('@rainbow-me/hooks', () => ({
   useWallets: () => ({ selectedWallet: 'fooSelectedWallet' }),
   useAssetListData: () => ({ isLoadingAssets: false }),
+  useAccountSettings: () => ({ accountAddress: 'foo' }),
 }));
 
 jest.mock('@rainbow-me/redux/hooks', () => ({
