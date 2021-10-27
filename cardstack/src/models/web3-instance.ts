ce0b18044d9fcfd143f7871c2b4b2f368ebd4085
danibonilha <dani.bonilha@live.com>
2021-10-27 14:26:09 -0300

    [CS-2239]: Set provider if ws disconnects

--- cardstack/src/models/web3-instance.ts
+++ cardstack/src/models/web3-instance.ts
@@ -1,4 +1,5 @@
 import Web3 from 'web3';
+import { WebsocketProvider } from 'web3-core';
 import HDProvider, { SignedProviderParams } from './hd-provider';
 import Web3WsProvider from './web3-provider';
 import logger from 'logger';
@@ -7,8 +8,11 @@ const web3Instance: Web3 = new Web3();
 
 const Web3Instance = {
   get: async (signedProviderParams?: SignedProviderParams) => {
+    const isProviderDisconnected = !(web3Instance.currentProvider as WebsocketProvider)
+      ?.connected;
+
     try {
-      if (web3Instance.currentProvider === null) {
+      if (web3Instance.currentProvider === null || isProviderDisconnected) {
         web3Instance.setProvider(await Web3WsProvider.get());
       }
 
