7301c00cec4fde19713398d3760a012c72b66c92
Eduardo Shinkawa <eshinkawa@gmail.com>
2021-10-29 19:05:58 -0300

    [CS-2315] - Add version to mobile app starting page (#458)

--- cardstack/src/screens/WelcomeScreen/strings.ts
+++ cardstack/src/screens/WelcomeScreen/strings.ts
@@ -1,6 +1,7 @@
 export const strings = {
   title: 'CARD WALLET',
-  subtitle: 'COMPANION APP TO THE\nCARDSTACK WEB APP',
+  subtitle: 'by Cardstack',
+  midtitle: 'COMPANION APP TO THE\nCARDSTACK WEB APP',
   newAccBtn: 'Create a new account',
   existingAccBtn: 'Add an existing account',
 };
