46a87ce22304233cc0f78136fa54dca6f883d84a
Luke Melia <luke@lukemelia.com>
2021-11-08 13:42:17 -0500

    Be resilient to wallet-connect peerMeta with no icons
    - this can occur when connecting to the dapp running on localhost in development mode

--- src/hooks/useWalletConnectConnections.js
+++ src/hooks/useWalletConnectConnections.js
@@ -11,7 +11,7 @@ import {
 const formatDappData = connections =>
   values(
     mapValues(connections, connection => ({
-      dappIcon: connection?.[0].peerMeta.icons[0],
+      dappIcon: connection?.[0].peerMeta.icons?.[0],
       dappName: connection?.[0].peerMeta.name,
       dappUrl: connection?.[0].peerMeta.url,
     }))
