98fdebcc6812a87e71a76694077943a0fc85e4e9
danibonilha <dani.bonilha@live.com>
2021-11-09 11:14:52 -0300

    [CS-2424]: Chop off cpxd token to get hist price

--- cardstack/src/services/historical-pricing-service.ts
+++ cardstack/src/services/historical-pricing-service.ts
@@ -1,8 +1,6 @@
 import { CRYPTOCOMPARE_API_KEY } from 'react-native-dotenv';
-import { getNativeBalanceFromOracle } from './exchange-rate-service';
 import logger from 'logger';
-import { toWei } from '@rainbow-me/handlers/web3';
-import { isCPXDToken } from '@cardstack/utils';
+import { removeCPXDTokenSuffix } from '@cardstack/utils';
 
 const getRoundedTimestamp = (timestamp: string | number) => {
   const date = new Date(Number(timestamp) * 1000);
@@ -18,24 +16,17 @@ export const fetchHistoricalPrice = async (
   nativeCurrency: string
 ) => {
   try {
-    if (isCPXDToken(symbol)) {
-      const priceFromOracle = await getNativeBalanceFromOracle({
-        symbol,
-        nativeCurrency,
-        balance: toWei('1'),
-      });
-
-      return priceFromOracle;
-    }
+    // For cpxd tokens we chop off .CPXD in order to get mainnet's historical price
+    const tokenSymbol = removeCPXDTokenSuffix(symbol);
 
     const roundedTimestamp = getRoundedTimestamp(timestamp);
 
     const response = await fetch(
-      `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${symbol}&tsyms=${nativeCurrency}&ts=${roundedTimestamp}&api_key=${CRYPTOCOMPARE_API_KEY}`
+      `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${tokenSymbol}&tsyms=${nativeCurrency}&ts=${roundedTimestamp}&api_key=${CRYPTOCOMPARE_API_KEY}`
     );
 
     const data = await response.json();
-    const price = data[symbol][nativeCurrency];
+    const price = data[tokenSymbol][nativeCurrency];
 
     return price;
   } catch (e) {
