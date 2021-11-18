b0da83312d6d395185679aa6c087e5aed78f58f5
danibonilha <dani.bonilha@live.com>
2021-11-18 19:35:17 -0300

    [Refact]: Update payM to use rtk query and mutation

--- cardstack/src/hooks/merchant/usePaymentMerchantUniversalLink.ts
+++ cardstack/src/hooks/merchant/usePaymentMerchantUniversalLink.ts
@@ -1,29 +1,17 @@
 import { useEffect, useMemo, useState } from 'react';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Alert, AlertButton, InteractionManager } from 'react-native';
-import { getSDK, MerchantSafe } from '@cardstack/cardpay-sdk';
-import { PrepaidCard } from '@cardstack/cardpay-sdk/sdk/prepaid-card';
-import { TransactionReceipt } from 'web3-core';
+import { MerchantSafe } from '@cardstack/cardpay-sdk';
 import {
   PayMerchantDecodedData,
-  PrepaidCardType,
   TransactionConfirmationType,
 } from '@cardstack/types';
-import { getSafeData, syncPrepaidCardFaceValue } from '@cardstack/services';
-import { useWorker } from '@cardstack/utils';
+import { getSafeData } from '@cardstack/services';
+import { isLayer1, useWorker } from '@cardstack/utils';
 import { Network } from '@rainbow-me/helpers/networkTypes';
 import { networkInfo } from '@rainbow-me/helpers/networkInfo';
-import { useRainbowSelector } from '@rainbow-me/redux/hooks';
-import { fetchAssetsBalancesAndPrices } from '@rainbow-me/redux/fallbackExplorer';
-import {
-  useAccountSettings,
-  useAssetListData,
-  useWallets,
-} from '@rainbow-me/hooks';
-import Web3Instance from '@cardstack/models/web3-instance';
-import HDProvider from '@cardstack/models/hd-provider';
-import { useLoadingOverlay } from '@cardstack/navigation';
-import logger from 'logger';
+import { useAccountSettings } from '@rainbow-me/hooks';
+import { useGetSafesDataQuery } from '@cardstack/services/safes';
 
 interface RouteType {
   params: {
@@ -47,24 +35,34 @@ export const usePaymentMerchantUniversalLink = () => {
     params: { merchantAddress, amount = '0', network: qrCodeNetwork, currency },
   } = useRoute<RouteType>();
 
-  const { showLoadingOverlay, dismissLoadingOverlay } = useLoadingOverlay();
-
   const { goBack } = useNavigation();
 
   const currencyName = currency || 'SPD';
 
   const [infoDID, setInfoDID] = useState<string | undefined>();
 
-  const prepaidCards: PrepaidCardType[] = useRainbowSelector(
-    state => state.data.prepaidCards
+  const {
+    accountAddress,
+    network: accountNetwork,
+    nativeCurrency,
+  } = useAccountSettings();
+
+  const { isLoadingCards, prepaidCards } = useGetSafesDataQuery(
+    { address: accountAddress, nativeCurrency },
+    {
+      refetchOnMountOrArgChange: 60,
+      skip: isLayer1(accountNetwork) || !accountAddress,
+      selectFromResult: ({ data, isLoading }) => ({
+        prepaidCards: data?.prepaidCards || [],
+        isLoadingCards: isLoading,
+      }),
+    }
   );
 
-  const { isLoadingAssets } = useAssetListData();
-
-  const { selectedWallet } = useWallets();
-  const { accountAddress, network: accountNetwork } = useAccountSettings();
-
-  const { isLoading, callback: getMerchantSafeData } = useWorker(async () => {
+  const {
+    isLoading: isLoadingMerchantInfo,
+    callback: getMerchantSafeData,
+  } = useWorker(async () => {
     const { infoDID: did } = (await getSafeData(
       merchantAddress
     )) as MerchantSafe;
@@ -72,24 +70,29 @@ export const usePaymentMerchantUniversalLink = () => {
     setInfoDID(did);
   }, [merchantAddress]);
 
+  const isLoading = !infoDID || isLoadingMerchantInfo || isLoadingCards;
+
   useEffect(() => {
     getMerchantSafeData();
   }, [getMerchantSafeData]);
 
   useEffect(() => {
-    if (!isLoadingAssets && prepaidCards.length === 0) {
+    if (!isLoading && prepaidCards.length === 0) {
       handleAlertError(
         `You don't own a Prepaid card!\nYou can create one at app.cardstack.com`
       );
 
       goBack();
-
-      return;
     }
-  }, [isLoadingAssets, goBack, prepaidCards.length]);
+  }, [isLoadingCards, goBack, prepaidCards.length, isLoading]);
 
   useEffect(() => {
-    if (qrCodeNetwork && accountNetwork && qrCodeNetwork !== accountNetwork) {
+    if (
+      !isLoading &&
+      qrCodeNetwork &&
+      accountNetwork &&
+      qrCodeNetwork !== accountNetwork
+    ) {
       InteractionManager.runAfterInteractions(() => {
         handleAlertError(
           `This is a ${networkInfo[qrCodeNetwork].name} QR Code, please confirm your device is on ${networkInfo[qrCodeNetwork].name}.`,
@@ -105,84 +108,23 @@ export const usePaymentMerchantUniversalLink = () => {
 
       return;
     }
-  }, [accountNetwork, qrCodeNetwork, goBack]);
+  }, [accountNetwork, qrCodeNetwork, goBack, isLoading, isLoadingCards]);
 
-  const { isLoading: isLoadingTx, callback: onConfirm, error } = useWorker(
-    async (
-      updatedSpendAmount: number,
-      prepaidCardAddress: string,
-      onSuccess: (receipt: TransactionReceipt) => void
-    ) => {
-      showLoadingOverlay({
-        title: 'Processing Transaction',
-        subTitle: `This will take approximately\n10-15 seconds`,
-      });
-
-      const web3 = await Web3Instance.get({
-        selectedWallet,
-        network: qrCodeNetwork,
-      });
-
-      const prepaidCardInstance: PrepaidCard = await getSDK(
-        'PrepaidCard',
-        web3
-      );
-
-      const receipt = await prepaidCardInstance.payMerchant(
-        merchantAddress,
-        prepaidCardAddress,
-        updatedSpendAmount,
-        undefined,
-        { from: accountAddress }
-      );
-
-      // update prepaidcard facevalue almost instantly
-      await syncPrepaidCardFaceValue(prepaidCardAddress);
-
-      // refetch all assets to sync
-      await fetchAssetsBalancesAndPrices();
-
-      // resets signed provider and web3 instance to kill poller
-      await HDProvider.reset();
-
-      onSuccess(receipt);
-    },
-    [
-      merchantAddress,
-      prepaidCards.length,
-      accountAddress,
-      showLoadingOverlay,
-      selectedWallet,
-    ]
-  );
-
-  useEffect(() => {
-    if (error) {
-      dismissLoadingOverlay();
-      logger.sentry('Pay Merchant failed!', error);
-      handleAlertError(
-        'Something unexpected happened! Please try again. If this error persists please contact support@cardstack.com'
-      );
-    }
-  }, [dismissLoadingOverlay, error]);
-
-  const data: PayMerchantDecodedData = useMemo(
+  const data: PayMerchantDecodedData & { qrCodeNetwork: string } = useMemo(
     () => ({
       type: TransactionConfirmationType.PAY_MERCHANT,
       infoDID,
+      qrCodeNetwork,
       amount: parseFloat(amount),
       merchantSafe: merchantAddress,
       currency: currencyName,
     }),
-    [infoDID, amount, merchantAddress, currencyName]
+    [infoDID, qrCodeNetwork, amount, merchantAddress, currencyName]
   );
 
   return {
     prepaidCards,
-    goBack,
-    onConfirm,
-    isLoadingTx,
-    isLoading: isLoading || isLoadingAssets,
+    isLoading,
     data,
   };
 };
--- cardstack/src/services/safes.ts
+++ cardstack/src/services/safes.ts
@@ -1,6 +1,9 @@
+import { getSDK } from '@cardstack/cardpay-sdk';
 import { NativeCurrency } from '@cardstack/cardpay-sdk/sdk/currencies';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { fetchSafes } from './gnosis-service';
+import HDProvider from '@cardstack/models/hd-provider';
+import Web3Instance from '@cardstack/models/web3-instance';
 
 export const safesApi = createApi({
   reducerPath: 'safesApi',
@@ -17,7 +20,41 @@ export const safesApi = createApi({
       },
       providesTags: ['SAFES'],
     }),
+    // TODO: Add right types, split endpoints and extract to own service
+    payMerchant: builder.mutation<any, any>({
+      async queryFn({
+        selectedWallet,
+        network,
+        merchantAddress,
+        prepaidCardAddress,
+        spendAmount,
+        accountAddress,
+      }) {
+        const web3 = await Web3Instance.get({
+          selectedWallet,
+          network,
+        });
+
+        const prepaidCardInstance = await getSDK('PrepaidCard', web3);
+
+        const receipt = await prepaidCardInstance.payMerchant(
+          merchantAddress,
+          prepaidCardAddress,
+          spendAmount,
+          undefined,
+          { from: accountAddress }
+        );
+
+        await HDProvider.reset();
+
+        // TODO: create wrapper to data and error
+        return receipt
+          ? { data: receipt }
+          : { error: { status: 418, data: '' } };
+      },
+      invalidatesTags: ['SAFES'],
+    }),
   }),
 });
 
-export const { useGetSafesDataQuery } = safesApi;
+export const { useGetSafesDataQuery, usePayMerchantMutation } = safesApi;
