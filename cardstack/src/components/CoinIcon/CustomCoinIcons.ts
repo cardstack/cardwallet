9e5e37f2b6b16aa942408d34ef875fa8c062f4ad
Brian <brianwebtek@gmail.com>
2021-11-23 01:56:45 +0800

    Get rid of from mainnet token icons

--- cardstack/src/components/CoinIcon/CustomCoinIcons.ts
+++ cardstack/src/components/CoinIcon/CustomCoinIcons.ts
@@ -1,9 +1,7 @@
 import CARD_ICON from '../../assets/coins/card.png';
 import CARDCPXD_ICON from '../../assets/coins/card.cpxd.png';
-import CARDMAINNET_ICON from '../../assets/coins/card_from_mainnet.png';
 import DAICPXD_ICON from '../../assets/coins/dai.cpxd.png';
 import XDAI_ICON from '../../assets/coins/xdai.png';
-import DAIMAINNET_ICON from '../../assets/coins/dai_from_mainnet.png';
 
 // coin icons list
 export default [
@@ -22,13 +20,5 @@ export default [
   {
     symbol: 'XDAI',
     icon: XDAI_ICON,
-  },
-  {
-    symbol: 'CARD.MAINNET',
-    icon: CARDMAINNET_ICON,
-  },
-  {
-    symbol: 'DAI.MAINNET',
-    icon: DAIMAINNET_ICON,
   },
 ];
