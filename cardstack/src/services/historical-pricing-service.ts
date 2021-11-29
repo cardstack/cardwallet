25579a4b012611fb1b8ca58da6cd811fd9eb79d1
Brian <brianwebtek@gmail.com>
2021-11-29 20:40:38 +0800

    Update fetchHistoricalPrice to support CARD token, fix unix timestamp

--- cardstack/src/services/historical-pricing-service.ts
+++ cardstack/src/services/historical-pricing-service.ts
@@ -2,14 +2,6 @@ import { CRYPTOCOMPARE_API_KEY } from 'react-native-dotenv';
 import logger from 'logger';
 import { removeCPXDTokenSuffix } from '@cardstack/utils';
 
-const getRoundedTimestamp = (timestamp: string | number) => {
-  const date = new Date(Number(timestamp) * 1000);
-
-  date.setHours(0, 0, 0, 0);
-
-  return date.getTime();
-};
-
 export const fetchHistoricalPrice = async (
   symbol: string,
   timestamp: string | number,
@@ -19,10 +11,31 @@ export const fetchHistoricalPrice = async (
     // For cpxd tokens we chop off .CPXD in order to get mainnet's historical price
     const tokenSymbol = removeCPXDTokenSuffix(symbol);
 
-    const roundedTimestamp = getRoundedTimestamp(timestamp);
+    // cryptocompare does not support CARD price history correctly,
+    // so uses kucoin exchange to get CARD price in USDT(kucoin supports only USDT) and convert it to native currency
+    if (tokenSymbol === 'CARD') {
+      const usdtPriceData = await (
+        await fetch(
+          `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${tokenSymbol}&tsyms=USDT&ts=${timestamp}&e=kucoin&api_key=${CRYPTOCOMPARE_API_KEY}`
+        )
+      ).json();
+
+      const usdtPrice = usdtPriceData.CARD.USDT;
+
+      const nativeCurrencyDataForUSDT = await (
+        await fetch(
+          `https://min-api.cryptocompare.com/data/pricehistorical?fsym=USDT&tsyms=${nativeCurrency}&ts=${timestamp}&api_key=${CRYPTOCOMPARE_API_KEY}`
+        )
+      ).json();
+
+      const nativeCurrencyPriceForUSDT =
+        nativeCurrencyDataForUSDT.USDT[nativeCurrency];
+
+      return usdtPrice * nativeCurrencyPriceForUSDT;
+    }
 
     const response = await fetch(
-      `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${tokenSymbol}&tsyms=${nativeCurrency}&ts=${roundedTimestamp}&api_key=${CRYPTOCOMPARE_API_KEY}`
+      `https://min-api.cryptocompare.com/data/pricehistorical?fsym=${tokenSymbol}&tsyms=${nativeCurrency}&ts=${timestamp}&api_key=${CRYPTOCOMPARE_API_KEY}`
     );
 
     const data = await response.json();
