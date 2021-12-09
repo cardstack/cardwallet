3c09247f4fff498144be240917a6e833812e0d77
Brian <brianwebtek@gmail.com>
2021-12-10 01:59:31 +0800

    Refactor firebase model with storing token to asyncstorage mechanism

--- src/model/firebase.ts
+++ src/model/firebase.ts
@@ -1,44 +1,80 @@
-import messaging from '@react-native-firebase/messaging';
+import messaging, {
+  FirebaseMessagingTypes,
+} from '@react-native-firebase/messaging';
 import lang from 'i18n-js';
 import { get } from 'lodash';
 import { requestNotifications } from 'react-native-permissions';
 import { Alert } from '../components/alerts';
 import { getLocal, saveLocal } from '../handlers/localstorage/common';
-import { getHubAuthToken } from '@cardstack/services/hub-service';
+import { registerFcmToken } from '@cardstack/services/hub-service';
+import { loadAddress } from '@rainbow-me/model/wallet';
 import logger from 'logger';
 
-export const getFCMToken = async (): Promise<string> => {
-  const fcmTokenLocal = await getLocal('rainbowFcmToken');
+const DEVICE_FCM_TOKEN_KEY = 'cardwalletFcmToken';
+
+const getPermissionStatus = (): Promise<FirebaseMessagingTypes.AuthorizationStatus> =>
+  messaging().hasPermission();
+
+export const getFCMToken = async (): Promise<{
+  fcmToken: string | null;
+  addresses: string[];
+}> => {
+  const fcmTokenLocal = await getLocal(DEVICE_FCM_TOKEN_KEY);
 
   const fcmToken: string = get(fcmTokenLocal, 'data', null);
 
   if (!fcmToken) {
-    throw new Error('Push notification token unavailable.');
+    return { fcmToken: null, addresses: [] };
   }
-  return fcmToken;
+  const addresses: string[] = get(fcmTokenLocal, 'addresses', []);
+  return { fcmToken, addresses };
 };
 
-const getPermissionStatus = () => messaging().hasPermission();
+// check if token's stored by confirming addresses includes wallet address
+export const isFCMTokenStored = async (
+  walletAddress: string
+): Promise<{
+  isTokenStored: boolean;
+  addresses: string[];
+  fcmToken: string | null;
+}> => {
+  const { fcmToken, addresses } = await getFCMToken();
+  return {
+    isTokenStored: !!fcmToken && addresses.includes(walletAddress),
+    fcmToken,
+    addresses,
+  };
+};
 
-export const saveFCMToken = async () => {
+// check if token is registered in hub with checking stored in asyncStorage associated with wallet address
+// and if not stored, register to hub, then update asyncStorage)
+export const saveFCMToken = async (
+  walletAddress: string,
+  seedPhrase?: string
+) => {
   try {
-    const permissionStatus = await getPermissionStatus();
-    if (
-      permissionStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      permissionStatus === messaging.AuthorizationStatus.PROVISIONAL
-    ) {
-      const fcmToken = await messaging().getToken();
-      const oldFcmToken = await getFCMToken();
+    const { isTokenStored, addresses, fcmToken } = await isFCMTokenStored(
+      walletAddress
+    );
+    if (!isTokenStored) {
+      const newFcmToken = await messaging().getToken();
+      await registerFcmToken(newFcmToken, walletAddress, seedPhrase);
 
-      // TODO: TESTING
-      const token = await getHubAuthToken();
-      console.log('token', token, fcmToken);
-      if (oldFcmToken !== fcmToken) {
-        saveLocal('rainbowFcmToken', { data: fcmToken });
+      // check if newFcmToken is same as old stored one and if same, add wallet address, otherwise replace addresses value with [walletAddress]
+      if (fcmToken !== newFcmToken) {
+        saveLocal(DEVICE_FCM_TOKEN_KEY, {
+          data: newFcmToken,
+          addresses: [walletAddress],
+        });
+      } else {
+        saveLocal(DEVICE_FCM_TOKEN_KEY, {
+          data: newFcmToken,
+          addresses: [...addresses, walletAddress],
+        });
       }
     }
   } catch (error) {
-    logger.log('error getting fcm token - cannot save', error);
+    logger.sentry('error fcm token - cannot register fcm token', error);
   }
 };
 
@@ -52,13 +88,16 @@ export const requestPermission = () => {
   });
 };
 
-export const checkPushNotificationPermissions = async () => {
+export const checkPushPermissionAndRegisterToken = async (
+  walletAddress: string,
+  seedPhrase?: string
+) => {
   return new Promise(async resolve => {
     let permissionStatus = null;
     try {
       permissionStatus = await getPermissionStatus();
     } catch (error) {
-      logger.log(
+      logger.sentry(
         'Error checking if a user has push notifications permission',
         error
       );
@@ -74,9 +113,9 @@ export const checkPushNotificationPermissions = async () => {
             onPress: async () => {
               try {
                 await requestPermission();
-                await saveFCMToken();
+                await saveFCMToken(walletAddress, seedPhrase);
               } catch (error) {
-                logger.log('User rejected push notifications permissions');
+                logger.sentry('User rejected push notifications permissions');
               } finally {
                 resolve(true);
               }
@@ -95,12 +134,27 @@ export const checkPushNotificationPermissions = async () => {
         title: lang.t('wallet.push_notifications.please_enable_title'),
       });
     } else {
+      await saveFCMToken(walletAddress, seedPhrase);
       resolve(true);
     }
   });
 };
 
 export const registerTokenRefreshListener = () =>
-  messaging().onTokenRefresh(fcmToken => {
-    saveLocal('rainbowFcmToken', { data: fcmToken });
+  messaging().onTokenRefresh(async fcmToken => {
+    try {
+      const tokenRegisterResponse = await registerFcmToken(fcmToken);
+      if (tokenRegisterResponse) {
+        const walletAddress = (await loadAddress()) || '';
+        saveLocal(DEVICE_FCM_TOKEN_KEY, {
+          data: fcmToken,
+          addresses: [walletAddress],
+        });
+      }
+    } catch (error) {
+      logger.sentry(
+        'registerTokenRefreshListener - cannot register refreshed fcm token',
+        error
+      );
+    }
   });
