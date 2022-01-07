3fea195db437dac6e178a1a276c4c4df9e8964bb
danibonilha <dani.bonilha@live.com>
2022-01-07 18:21:00 -0300

    [Cleanup]: Remove unused buttons

--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -5,7 +5,4 @@ export {
   HoldToAuthorizeButton,
   HoldToAuthorizeButtonIcon,
 } from './hold-to-authorize';
-export { default as MiniButton } from './MiniButton';
 export { default as PasteAddressButton } from './PasteAddressButton';
-export { default as RainbowButton } from './rainbow-button/RainbowButton';
-export { default as TokenSelectionButton } from './TokenSelectionButton';
