aa68ea5b35bcafd6e08c9be79bab8f18aa0f71d8
Brian <brianwebtek@gmail.com>
2022-01-12 17:01:02 +0800

    Update SendAssetFormField UI

--- src/components/send/SendAssetFormField.js
+++ src/components/send/SendAssetFormField.js
@@ -1,10 +1,8 @@
 import React, { useCallback } from 'react';
 
 import { UnderlineField } from '../fields';
-import { CenteredContainer, Text } from '@cardstack/components';
+import { Button, CenteredContainer, Text } from '@cardstack/components';
 
-// set currency label with fixed width to have same width inputs. 100 is good for longest currency label for now
-const CURRENCY_LABEL_WIDTH = 100;
 export default function SendAssetFormField({
   autoFocus,
   format,
@@ -19,6 +17,8 @@ export default function SendAssetFormField({
   testID,
   ...props
 }) {
+  const [isFocused, setIsFocused] = useState(autoFocus);
+
   const handlePressButton = useCallback(
     event => {
       onPressButton?.(event);
@@ -26,24 +26,41 @@ export default function SendAssetFormField({
     [onPressButton]
   );
 
+  const handleBlur = useCallback(() => {
+    setIsFocused(false);
+  }, []);
+
+  const handleFocus = useCallback(
+    event => {
+      setIsFocused(true);
+      onFocus?.(event);
+    },
+    [onFocus]
+  );
+
   return (
-    <CenteredContainer flexDirection="row" {...props}>
+    <CenteredContainer {...props} flexDirection="row">
       <UnderlineField
         autoFocus={autoFocus}
-        buttonText="Max"
+        flex={1}
         flexGrow={1}
         format={format}
         keyboardType="decimal-pad"
         mask={mask}
+        onBlur={handleBlur}
         onChange={onChange}
-        onFocus={onFocus}
-        onPressButton={handlePressButton}
+        onFocus={handleFocus}
         placeholder={placeholder}
         testID={testID}
         value={value}
       />
-      <CenteredContainer width={CURRENCY_LABEL_WIDTH}>
-        <Text fontSize={20} fontWeight="700">
+      <CenteredContainer flexDirection="row" position="absolute" right={0}>
+        {isFocused && (
+          <Button onPress={handlePressButton} variant="tiny">
+            Max
+          </Button>
+        )}
+        <Text fontSize={20} fontWeight="700" lineHeight={24} paddingLeft={2}>
           {label?.length > labelMaxLength
             ? label.substring(0, labelMaxLength)
             : label}
