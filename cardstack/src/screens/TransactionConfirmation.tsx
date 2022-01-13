9cbbba1d8a7b0e729eb38de60760e4e7a1829f71
Brian <brianwebtek@gmail.com>
2022-01-14 01:08:48 +0800

    Fix confirmation sheet hide statusBar issue in android

--- cardstack/src/screens/TransactionConfirmation.tsx
+++ cardstack/src/screens/TransactionConfirmation.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { GasSpeedButton } from '../../../src/components/gas';
 import { useTransactionConfirmation } from '@cardstack/hooks';
+import { Device } from '@cardstack/utils';
 import {
   Container,
   TransactionConfirmationSheet,
   SafeAreaView,
 } from '@cardstack/components';
+
+// converted Status bar height that restyle paddingTop prop can understand
+const StatusBarHeightInStyle = Math.ceil((StatusBar.currentHeight || 24) / 4);
 
 const TransactionConfirmation = () => {
   const {
@@ -21,7 +26,13 @@ const TransactionConfirmation = () => {
   } = useTransactionConfirmation();
 
   return (
-    <SafeAreaView backgroundColor="black" flex={1} width="100%">
+    <SafeAreaView
+      backgroundColor="black"
+      flex={1}
+      width="100%"
+      paddingTop={Device.isAndroid ? StatusBarHeightInStyle : 0}
+    >
+      <StatusBar barStyle="light-content" />
       <TransactionConfirmationSheet
         data={data}
         dappUrl={dappUrl}
