e909c32f29d1a65782cbea31e39795c704fa3893
Douglas Schmidt <douglaslondrina@gmail.com>
2022-01-17 17:27:16 -0300

    Added apple cash cards not supported text to Support & Fees screen

--- src/components/expanded-state/SupportAndFeedsState.tsx
+++ src/components/expanded-state/SupportAndFeedsState.tsx
@@ -22,7 +22,7 @@ const strings = {
         info: '$1,000 per week  max equivalent, $7,500 yearly max equivalent',
       },
     },
-    footerInfo: `Buy Prepaid Card is not available in NY & TX\nWyre currently only supports Visa or Mastercard`,
+    footerInfo: `Buy Prepaid Card is not available in NY & TX\nWyre currently only supports Visa or Mastercard\n\nApple Cash Cards are not supported at this time`,
   },
   supportedCountriesTitle: 'Supported Countries',
 };
