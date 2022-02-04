b17b5a590558b45a017edf2a37c4012b6050c90e
Brian <brianwebtek@gmail.com>
2022-02-04 23:26:17 +0800

    Add loading animation when scanning is disabled and in progress

--- src/components/qrcode-scanner/QRCodeScannerCrosshair.js
+++ src/components/qrcode-scanner/QRCodeScannerCrosshair.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { useIsEmulator } from 'react-native-device-info';
 import { CenteredContainer, Icon, Text } from '@cardstack/components';
 
@@ -6,7 +7,7 @@ import { useDimensions } from '@rainbow-me/hooks';
 
 const CrossHairAspectRatio = 259 / 375;
 
-export default function QRCodeScannerCrosshair() {
+export default function QRCodeScannerCrosshair({ isScanningEnabled }) {
   const { width: deviceWidth } = useDimensions();
   const { result: isEmulator } = useIsEmulator();
   const size = deviceWidth * CrossHairAspectRatio;
@@ -14,9 +15,13 @@ export default function QRCodeScannerCrosshair() {
   return (
     <CenteredContainer height={size} marginBottom={10} width={size} zIndex={1}>
       <Icon color="white" name="crosshair" position="absolute" size={size} />
-      <Text color="white" size="small" textAlign="center" weight="bold">
-        {isEmulator ? `Simulator Mode\n (Paste in URI Code)` : 'Scan QR Code'}
-      </Text>
+      {isScanningEnabled ? (
+        <Text color="white" size="small" textAlign="center" weight="bold">
+          {isEmulator ? `Simulator Mode\n (Paste in URI Code)` : 'Scan QR Code'}
+        </Text>
+      ) : (
+        <ActivityIndicator color="white" size="large" />
+      )}
     </CenteredContainer>
   );
 }
