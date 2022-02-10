d0889ebbac8a8528f957260cdda0cb76feab3aac
danibonilha <dani.bonilha@live.com>
2022-02-10 18:23:45 -0300

    [CS-3102]: Update TxList to use tabBar

--- cardstack/src/components/TransactionList/TransactionList.tsx
+++ cardstack/src/components/TransactionList/TransactionList.tsx
@@ -1,49 +1,23 @@
-import React, { memo, useCallback } from 'react';
+import React, { memo, useCallback, useEffect, useMemo, useRef } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
-import {
-  RefreshControl,
-  SectionList,
-  ActivityIndicator,
-  StyleSheet,
-} from 'react-native';
+import { RefreshControl, SectionList, ActivityIndicator } from 'react-native';
 
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
 import { TransactionListLoading } from './TransactionListLoading';
 import { useFullTransactionList } from '@cardstack/hooks';
-import { colors } from '@cardstack/theme';
 import {
   Container,
   ListEmptyComponent,
-  ScrollView,
   Text,
   TransactionItem,
+  TransactionItemProps,
 } from '@cardstack/components';
+import { useTabBarFlag } from '@cardstack/navigation/tabBarNavigator';
 
 interface TransactionListProps {
-  Header: JSX.Element;
+  Header?: JSX.Element;
   accountAddress: string;
 }
-
-const styles = StyleSheet.create({
-  contentContainerStyle: { paddingBottom: 40 },
-  background: { backgroundColor: colors.backgroundBlue },
-});
-
-const renderSectionHeader = ({
-  section: { title },
-}: {
-  section: { title: string };
-}) => (
-  <Container
-    paddingVertical={2}
-    paddingHorizontal={5}
-    width="100%"
-    backgroundColor="backgroundBlue"
-  >
-    <Text size="medium" color="white">
-      {title}
-    </Text>
-  </Container>
-);
 
 export const TransactionList = memo(({ Header }: TransactionListProps) => {
   const {
@@ -55,39 +29,79 @@ export const TransactionList = memo(({ Header }: TransactionListProps) => {
     refetchLoading,
   } = useFullTransactionList();
 
+  const isLoadingFallback = useRef(true);
+
+  useEffect(() => {
+    if (isLoadingTransactions) {
+      // Once tx are loading we don't need to track anymore
+      isLoadingFallback.current = false;
+    }
+  }, [isLoadingTransactions]);
+
+  const { isTabBarEnabled } = useTabBarFlag();
+
+  // TODO: Remove condition after tab is official
+  // useBottomTabBarHeight throws error if it's not inside tabNav
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const tabBarHeight = isTabBarEnabled ? useBottomTabBarHeight() : 0;
+
+  const contentContainerStyle = useMemo(
+    () => ({
+      paddingBottom: 40 + tabBarHeight,
+    }),
+    [tabBarHeight]
+  );
+
+  const renderSectionHeader = useCallback(
+    ({ section: { title } }: { section: { title: string } }) => (
+      <Container
+        paddingVertical={2}
+        paddingHorizontal={5}
+        width="100%"
+        backgroundColor={
+          isTabBarEnabled ? 'backgroundDarkPurple' : 'backgroundBlue'
+        }
+      >
+        <Text size="medium" color="white">
+          {title}
+        </Text>
+      </Container>
+    ),
+    [isTabBarEnabled]
+  );
+
   const onRefresh = useCallback(() => {
     refetch && refetch();
   }, [refetch]);
 
   useFocusEffect(onRefresh);
 
-  if (isLoadingTransactions) {
-    return (
-      <ScrollView
-        backgroundColor="backgroundBlue"
-        contentContainerStyle={styles.contentContainerStyle}
-      >
-        {Header}
-        <TransactionListLoading />
-      </ScrollView>
-    );
-  }
+  const renderItem = useCallback(
+    (props: TransactionItemProps) => <TransactionItem {...props} />,
+    []
+  );
 
   return (
     <SectionList
       ListEmptyComponent={
-        <ListEmptyComponent
-          text={`You don't have any\ntransactions yet`}
-          textColor="blueText"
-          hasRoundBox
-        />
+        // Use fallback to avoid flickering empty component,
+        // when fetching hasn't started yet
+        isLoadingTransactions || isLoadingFallback ? (
+          <TransactionListLoading />
+        ) : (
+          <ListEmptyComponent
+            text={`You don't have any\ntransactions yet`}
+            textColor="blueText"
+            hasRoundBox
+          />
+        )
       }
       ListHeaderComponent={Header}
       ListFooterComponent={
         isFetchingMore ? <ActivityIndicator color="white" /> : null
       }
-      contentContainerStyle={styles.contentContainerStyle}
-      renderItem={props => <TransactionItem {...props} />}
+      contentContainerStyle={contentContainerStyle}
+      renderItem={renderItem}
       sections={sections}
       renderSectionHeader={renderSectionHeader}
       refreshControl={
@@ -99,7 +113,6 @@ export const TransactionList = memo(({ Header }: TransactionListProps) => {
       }
       onEndReached={onEndReached}
       onEndReachedThreshold={1}
-      style={styles.background}
     />
   );
 });
