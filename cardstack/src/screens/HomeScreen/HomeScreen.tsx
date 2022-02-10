9727ce90fd2d0f0464d18be4e5748ab1cdcc9309
danibonilha <dani.bonilha@live.com>
2022-02-10 19:27:38 -0300

    [CS-3102]: Create homeScreen component

---
+++ cardstack/src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,39 @@
+import React, { useMemo } from 'react';
+
+import { Container, MainHeader, TransactionList } from '@cardstack/components';
+import { useAccountProfile } from '@rainbow-me/hooks';
+import {
+  DiscordPromoBanner,
+  useDiscordPromoBanner,
+} from '@cardstack/components/DiscordPromoBanner';
+
+const HomeScreen = () => {
+  const { accountAddress } = useAccountProfile();
+
+  const {
+    showPromoBanner,
+    onPress: onDiscordPromoPress,
+  } = useDiscordPromoBanner();
+
+  const renderPromoBanner = useMemo(
+    () =>
+      showPromoBanner ? (
+        <DiscordPromoBanner onPress={onDiscordPromoPress} />
+      ) : undefined,
+    [onDiscordPromoPress, showPromoBanner]
+  );
+
+  return (
+    <Container backgroundColor="backgroundDarkPurple" flex={1}>
+      <MainHeader title="ACTIVITY" />
+      <Container paddingTop={6}>
+        <TransactionList
+          Header={renderPromoBanner}
+          accountAddress={accountAddress}
+        />
+      </Container>
+    </Container>
+  );
+};
+
+export default HomeScreen;
