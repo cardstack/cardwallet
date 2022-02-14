0467afb2de4ee0f73dfb5c72788fb3fc2504ee00
danibonilha <dani.bonilha@live.com>
2022-02-14 16:31:36 -0300

    [CS-3115]: Use appVersion to trigger tabBar option

--- src/components/AppVersionStamp.tsx
+++ src/components/AppVersionStamp.tsx
@@ -1,28 +1,25 @@
 import React, { memo, useCallback, useRef } from 'react';
-import { Alert, TouchableWithoutFeedback } from 'react-native';
 
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { Text } from '@cardstack/components';
-import { useAppVersion, useWalletsDebug } from '@rainbow-me/hooks';
+import { useTabBarFlag } from '@cardstack/navigation/tabBarNavigator';
+import { useAppVersion } from '@rainbow-me/hooks';
 
 const VERSION_TAP_COUNT = 3;
 
 const AppVersionStamp = () => {
   const appVersion = useAppVersion();
   const numberOfTaps = useRef(0);
-  const debug = useWalletsDebug();
+
+  const { setIsTabBarEnabled, isTabBarEnabled } = useTabBarFlag();
 
   const handleVersionPress = useCallback(async () => {
     numberOfTaps.current++;
 
     if (numberOfTaps.current === VERSION_TAP_COUNT) {
-      const { status, data } = await debug();
-      if (status === 'restored') {
-        Alert.alert('Account restored successfully!', data);
-      } else {
-        Alert.alert('DEBUG INFO', data);
-      }
+      setIsTabBarEnabled(!isTabBarEnabled);
     }
-  }, [debug]);
+  }, [isTabBarEnabled, setIsTabBarEnabled]);
 
   return (
     <TouchableWithoutFeedback onPress={handleVersionPress}>
