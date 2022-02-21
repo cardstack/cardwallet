2bd96e0e7fdc30e34c3de7770fc14729bf2aa94c
Douglas Schmidt <douglaslondrina@gmail.com>
2022-02-21 10:22:22 -0300

    Adding supported font weight variations for font OpenSans-Regular on android.

--- cardstack/src/theme/fontWeights.ts
+++ cardstack/src/theme/fontWeights.ts
@@ -1,5 +1,14 @@
-export const fontWeights = {
-  regular: '400',
-  bold: '600',
-  extraBold: '700',
-};
+import { Platform } from 'react-native';
+
+export const fontWeights = Platform.select({
+  ios: {
+    regular: '400',
+    bold: '600',
+    extraBold: '700',
+  },
+  default: {
+    regular: 'normal',
+    bold: 'bold',
+    extraBold: 'bold',
+  },
+});
