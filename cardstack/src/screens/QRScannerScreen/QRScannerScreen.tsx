b476e76e56e10c4d0b02e2a779bd2204a082df24
Brian <brianwebtek@gmail.com>
2022-03-03 11:53:23 +0800

    Cleanup QRScanner screen

--- cardstack/src/screens/QRScannerScreen/QRScannerScreen.tsx
+++ cardstack/src/screens/QRScannerScreen/QRScannerScreen.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback } from 'react';
-import { FlatList } from 'react-native';
 import {
   CameraDimmer,
   EmulatorPasteUriButton,
@@ -11,16 +10,12 @@ import {
   HeaderHeight,
 } from '@rainbow-me/components/header';
 
-import { WalletConnectExplainer } from '@rainbow-me/components/walletconnect-list';
 import {
   CenteredContainer,
   Container,
-  ListItem,
-  Sheet,
   SwitchSelector,
 } from '@cardstack/components';
 
-import { useWalletConnectConnections } from '@rainbow-me/hooks';
 import { useNavigation } from '@rainbow-me/navigation';
 import Routes from '@rainbow-me/routes';
 const SHEET_HEIGHT = 240;
@@ -28,24 +23,9 @@ const SHEET_HEIGHT = 240;
 const QRScannerScreen = () => {
   const { navigate } = useNavigation();
 
-  const {
-    walletConnectorsByDappName,
-    walletConnectorsCount,
-    walletConnectDisconnectAllByDappName,
-  } = useWalletConnectConnections();
-
   const handlePressBackButton = useCallback(
     () => navigate(Routes.WALLET_SCREEN),
     [navigate]
-  );
-
-  const handlePressActionSheet = useCallback(
-    ({ dappName, index }) => {
-      if (index === 0) {
-        walletConnectDisconnectAllByDappName(dappName);
-      }
-    },
-    [walletConnectDisconnectAllByDappName]
   );
 
   return (
@@ -92,40 +72,6 @@ const QRScannerScreen = () => {
             contentPositionTop={HeaderHeight}
           />
         </CameraDimmer>
-
-        {/* <Container bottom={0} position="absolute" width="100%">
-          <Sheet borderRadius={20} hideHandle shadowEnabled>
-            {walletConnectorsCount ? (
-              <>
-                <FlatList
-                  alwaysBounceVertical={false}
-                  data={walletConnectorsByDappName}
-                  keyExtractor={item => `${item.dappUrl}`}
-                  removeClippedSubviews
-                  renderItem={({ item, index }) => (
-                    <>
-                      <ListItem
-                        actionSheetProps={{
-                          onPress: () => {
-                            handlePressActionSheet({ ...item, index });
-                          },
-                          options: ['Disconnect', 'Cancel'],
-                          title: `Would you like to disconnect from ${item.dappName}?`,
-                        }}
-                        avatarProps={{ source: item.dappIcon }}
-                        subText="Connected"
-                        title={item.dappName || ''}
-                      />
-                    </>
-                  )}
-                  scrollEventThrottle={32}
-                />
-              </>
-            ) : (
-              <WalletConnectExplainer />
-            )}
-          </Sheet>
-        </Container> */}
       </CenteredContainer>
     </Container>
   );
