12890ce5f74dc8da09068dfbdcea97c75e10b358
Brian <brianwebtek@gmail.com>
2022-03-07 20:49:28 +0800

    Update profile screen to have CTA to create a profile

--- cardstack/src/screens/ProfileScreen/ProfileScreen.tsx
+++ cardstack/src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-import { Container, MainHeader } from '@cardstack/components';
+import { Button, Container, MainHeader, Text } from '@cardstack/components';
+import { useRainbowSelector } from '@rainbow-me/redux/hooks';
 
 const ProfileScreen = () => {
+  // TODO: replace this with usePrimaryMerchant hook
+  const merchantSafes = useRainbowSelector(state => state.data.merchantSafes);
+  const hasSafes = (merchantSafes || []).length > 0;
+
+  // TODO: This is a temporary workaround as designs are pending yet
+  const NoSafesPlaceholder = () => (
+    <Container flex={1} justifyContent="center">
+      <Container
+        backgroundColor="black"
+        justifyContent="center"
+        alignItems="center"
+        paddingVertical={10}
+      >
+        <Text color="white" marginTop={10}>
+          Placeholder
+        </Text>
+        <Button variant="small" marginTop={10}>
+          Create Profile
+        </Button>
+      </Container>
+    </Container>
+  );
+
   return (
     <Container backgroundColor="backgroundDarkPurple" flex={1}>
       <MainHeader title="PROFILE" />
+      {!hasSafes && <NoSafesPlaceholder />}
     </Container>
   );
 };
