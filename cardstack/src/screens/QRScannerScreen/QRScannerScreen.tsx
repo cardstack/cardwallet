d76eefcecf42bd98a72ff1e43fd1fba8331b4ff6
danibonilha <dani.bonilha@live.com>
2022-03-10 18:58:37 -0300

    [CS-3313]: Add header on ScannerPage

--- cardstack/src/screens/QRScannerScreen/QRScannerScreen.tsx
+++ cardstack/src/screens/QRScannerScreen/QRScannerScreen.tsx
@@ -1,26 +1,78 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
+import { useIsEmulator } from 'react-native-device-info';
 import {
-  QRScannerContainer,
+  EmulatorPasteUriButton,
   RequestQRCode,
-  ScannerScreenMode,
+  useRequestCodePage,
 } from './components';
 import { QRCodeScannerPage } from './pages';
-import { SwitchSelectorOption } from '@cardstack/components';
+import {
+  Container,
+  Icon,
+  MainHeader,
+  SwitchSelector,
+  SwitchSelectorOption,
+} from '@cardstack/components';
+import { layoutEasingAnimation } from '@cardstack/utils';
+import { useTabBarFlag } from '@cardstack/navigation/tabBarNavigator';
+
+enum PageMode {
+  SCAN,
+  REQUEST,
+}
+
+const strings = {
+  scanLabel: 'Scan',
+  requestLabel: 'Request',
+};
+
+const Pages = [
+  { label: strings.scanLabel, value: PageMode.SCAN },
+  { label: strings.requestLabel, value: PageMode.REQUEST },
+];
 
 const QRScannerScreen = () => {
-  const [selectedScreen, selectScreen] = useState<ScannerScreenMode>(
-    ScannerScreenMode.SCAN
-  );
+  const [selectedScreen, selectScreen] = useState<PageMode>(PageMode.SCAN);
 
-  const onSwitchScreen = useCallback((option: SwitchSelectorOption) => {
+  const isScanSelected = useMemo(() => selectedScreen === PageMode.SCAN, [
+    selectedScreen,
+  ]);
+
+  const { result: isEmulator } = useIsEmulator();
+
+  const { isTabBarEnabled } = useTabBarFlag();
+
+  const toggleScreenContent = useCallback((option: SwitchSelectorOption) => {
+    layoutEasingAnimation();
     selectScreen(option.value);
   }, []);
 
+  const { handleShareLink, safeAddress } = useRequestCodePage();
+
+  const renderRightIcon = useMemo(
+    () =>
+      isEmulator && isScanSelected ? (
+        <EmulatorPasteUriButton />
+      ) : safeAddress ? (
+        <Icon name="share" size={24} onPress={handleShareLink} />
+      ) : undefined,
+    [handleShareLink, isEmulator, isScanSelected, safeAddress]
+  );
+
   return (
-    <QRScannerContainer onSwitchScreen={onSwitchScreen}>
-      {selectedScreen === ScannerScreenMode.SCAN && <QRCodeScannerPage />}
-      {selectedScreen === ScannerScreenMode.REQUEST && <RequestQRCode />}
-    </QRScannerContainer>
+    <Container flex={1} backgroundColor="backgroundDarkPurple">
+      <MainHeader
+        backgroundColor={!isTabBarEnabled ? 'transparent' : undefined}
+        rightIcon={renderRightIcon}
+      >
+        <SwitchSelector
+          options={Pages}
+          flex={0.7}
+          onPress={toggleScreenContent}
+        />
+      </MainHeader>
+      {isScanSelected ? <QRCodeScannerPage /> : <RequestQRCode />}
+    </Container>
   );
 };
 
