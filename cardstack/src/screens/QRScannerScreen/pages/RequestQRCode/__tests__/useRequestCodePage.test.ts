eeb2dd8b7fa24b2cf73f4e3b8054c803c7b2fe88
danibonilha <dani.bonilha@live.com>
2022-03-11 12:43:15 -0300

    [CS-3313]: Add tests to useRequest hook

---
+++ cardstack/src/screens/QRScannerScreen/pages/RequestQRCode/__tests__/useRequestCodePage.test.ts
@@ -0,0 +1,66 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+
+import { useRequestCodePage } from '../useRequestCodePage';
+import Routes from '@rainbow-me/routes';
+import usePrimarySafe from '@cardstack/redux/hooks/usePrimarySafe';
+
+const mockedNavigate = jest.fn();
+jest.mock('@react-navigation/core', () => ({
+  useNavigation: () => ({
+    navigate: mockedNavigate,
+  }),
+}));
+
+jest.mock('@cardstack/hooks/merchant/usePaymentLinks', () => ({
+  usePaymentLinks: () => ({
+    handleShareLink: jest.fn(),
+    paymentRequestDeepLink: 'fooLink',
+  }),
+}));
+
+jest.mock('@cardstack/redux/hooks/usePrimarySafe', () => jest.fn());
+
+const primarySafeMock = {
+  address: 'merchantAddress',
+  merchantInfo: { name: 'foo' },
+};
+
+describe('useRequestCodePage', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should navigate to payment request sheet on request amount press', async () => {
+    (usePrimarySafe as jest.Mock).mockImplementation(() => ({
+      primarySafe: primarySafeMock,
+    }));
+
+    const { result } = renderHook(useRequestCodePage);
+
+    act(() => {
+      result.current.onRequestAmountPress();
+    });
+
+    expect(mockedNavigate).toBeCalledWith(
+      Routes.MERCHANT_PAYMENT_REQUEST_SHEET,
+      {
+        address: primarySafeMock.address,
+        merchantInfo: primarySafeMock.merchantInfo,
+      }
+    );
+  });
+
+  it('should NOT navigate if no primary safe exists', async () => {
+    (usePrimarySafe as jest.Mock).mockImplementation(() => ({
+      primarySafe: undefined,
+    }));
+
+    const { result } = renderHook(useRequestCodePage);
+
+    act(() => {
+      result.current.onRequestAmountPress();
+    });
+
+    expect(mockedNavigate).not.toBeCalled();
+  });
+});
