02207f5bdfa6cfcb7d1976ea776882d137dc71c2
danibonilha <dani.bonilha@live.com>
2022-03-24 20:41:16 -0300

    [Fix]: Remove useless wrapper on expd to fix sheet

--- src/screens/ExpandedAssetSheet.js
+++ src/screens/ExpandedAssetSheet.js
@@ -1,10 +1,6 @@
 import { useRoute } from '@react-navigation/native';
-import React, { createElement } from 'react';
-import { StatusBar } from 'react-native';
-import { useSafeArea } from 'react-native-safe-area-context';
-import styled from 'styled-components';
+import { createElement } from 'react';
 
-import TouchableBackdrop from '../components/TouchableBackdrop';
 import {
   AvailableBalancesExpandedState,
   ChartExpandedState,
@@ -12,11 +8,8 @@ import {
   LiquidityPoolExpandedState,
   SupportAndFeedsState,
 } from '../components/expanded-state';
-import { Centered } from '../components/layout';
-import { useAsset, useDimensions } from '../hooks';
-import { useNavigation } from '../navigation/Navigation';
+import { useAsset } from '../hooks';
 import { ExpandedMerchantRoutes } from '@cardstack/components';
-import { position } from '@rainbow-me/styles';
 
 const ScreenTypes = {
   token: ChartExpandedState,
@@ -26,35 +19,14 @@ const ScreenTypes = {
   supportAndFees: SupportAndFeedsState,
 };
 
-const Container = styled(Centered).attrs({
-  direction: 'column',
-})`
-  ${position.cover};
-  ${({ deviceHeight, height }) =>
-    height ? `height: ${height + deviceHeight}` : null};
-`;
-
 export default function ExpandedAssetSheet(props) {
-  const { height: deviceHeight } = useDimensions();
-  const insets = useSafeArea();
-  const { goBack } = useNavigation();
   const { params } = useRoute();
 
   const selectedAsset = useAsset(params.asset);
 
-  return (
-    <Container
-      deviceHeight={deviceHeight}
-      height={params.longFormHeight}
-      insets={insets}
-    >
-      <StatusBar barStyle="light-content" />
-      {ios && <TouchableBackdrop onPress={goBack} />}
-      {createElement(ScreenTypes[params.type], {
-        asset: selectedAsset,
-        safeAddress: params?.safeAddress,
-        ...props,
-      })}
-    </Container>
-  );
+  return createElement(ScreenTypes[params.type], {
+    asset: selectedAsset,
+    safeAddress: params?.safeAddress,
+    ...props,
+  });
 }
