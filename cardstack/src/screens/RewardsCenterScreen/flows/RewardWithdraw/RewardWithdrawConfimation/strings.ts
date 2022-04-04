6a33ddfa7b3391f3c32d8e3d474f624df1b04231
Douglas Schmidt <douglaslondrina@gmail.com>
2022-04-04 12:51:16 -0300

    wip: Withdraw Confirmation

--- cardstack/src/screens/RewardsCenterScreen/flows/RewardWithdraw/RewardWithdrawConfimation/strings.ts
+++ cardstack/src/screens/RewardsCenterScreen/flows/RewardWithdraw/RewardWithdrawConfimation/strings.ts
@@ -1,3 +1,12 @@
 export const strings = {
   headerTitle: 'Withdraw Rewards',
+  withdraw: {
+    title: 'WITHDRAW THIS AMOUNT',
+  },
+  net: {
+    title: 'NET AMOUNT',
+    amount: 'AMOUNT',
+    estGas: 'EST. GAS AMOUNT',
+    estNet: 'EST. NET WITHDRAW',
+  },
 };
