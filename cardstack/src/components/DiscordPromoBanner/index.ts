f67bfb59248c58d0c14a4d28b31cc3aa5b8a69bc
Douglas Schmidt <douglaslondrina@gmail.com>
2022-04-06 10:39:32 -0300

    [CS-3583][CS-3582] Removes Discord Banner, Adds Rewards to WalletScreen (#770)
    * Removes Discord Banner from AssetList and HomeScreen.
    
    * Removes tabbar conditional for rewards,
    shows it on WalletScreen in tab bar more.
    
    * Adds padding to rewards banner
    
    * Changes padding for rewards banner.
    
    * Deletes DiscordPromoBanner component and assets.

--- cardstack/src/components/DiscordPromoBanner/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './DiscordPromoBanner';
-export * from './useDiscordPromoBanner';
--- cardstack/src/screens/HomeScreen/HomeScreen.tsx
+++ cardstack/src/screens/HomeScreen/HomeScreen.tsx
@@ -1,36 +1,16 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import { Container, MainHeader, TransactionList } from '@cardstack/components';
 import { useAccountProfile } from '@rainbow-me/hooks';
-import {
-  DiscordPromoBanner,
-  useDiscordPromoBanner,
-} from '@cardstack/components/DiscordPromoBanner';
 
 const HomeScreen = () => {
   const { accountAddress } = useAccountProfile();
-
-  const {
-    showPromoBanner,
-    onPress: onDiscordPromoPress,
-  } = useDiscordPromoBanner();
-
-  const renderPromoBanner = useMemo(
-    () =>
-      showPromoBanner ? (
-        <DiscordPromoBanner onPress={onDiscordPromoPress} />
-      ) : undefined,
-    [onDiscordPromoPress, showPromoBanner]
-  );
 
   return (
     <Container backgroundColor="backgroundDarkPurple" flex={1}>
       <MainHeader title="ACTIVITY" />
       <Container>
-        <TransactionList
-          Header={renderPromoBanner}
-          accountAddress={accountAddress}
-        />
+        <TransactionList accountAddress={accountAddress} />
       </Container>
     </Container>
   );
