c1aa1de75c5fe23cef8b43a87ecae74f11646e49
Brian <brianwebtek@gmail.com>
2022-04-06 20:38:04 +0800

    Update ProfileScreen to show message in mainnet

--- cardstack/src/screens/ProfileScreen/ProfileScreen.tsx
+++ cardstack/src/screens/ProfileScreen/ProfileScreen.tsx
@@ -2,27 +2,39 @@ import React, { useMemo, useCallback } from 'react';
 import { ActivityIndicator } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { CreateProfile, strings } from './components';
-import { Container, MainHeader, MerchantContent } from '@cardstack/components';
+import {
+  Container,
+  MainHeader,
+  MerchantContent,
+  Text,
+} from '@cardstack/components';
 import { usePrimarySafe } from '@cardstack/redux/hooks/usePrimarySafe';
+import { useAccountSettings } from '@rainbow-me/hooks';
+import { Network } from '@rainbow-me/helpers/networkTypes';
 
 const ProfileScreen = () => {
   const { primarySafe, isFetching, refetch, safesCount } = usePrimarySafe();
+  const { network } = useAccountSettings();
 
-  const ProfileBody = useMemo(
-    () =>
-      primarySafe ? (
-        <MerchantContent
-          showSafePrimarySelection={safesCount > 1}
-          isPrimarySafe
-          merchantSafe={primarySafe}
-          isRefreshingBalances={isFetching}
-          refetch={refetch}
-        />
-      ) : (
-        <CreateProfile />
-      ),
-    [primarySafe, isFetching, refetch, safesCount]
-  );
+  const ProfileBody = useMemo(() => {
+    if (!primarySafe && isFetching) {
+      return <ActivityIndicator size="large" color="white" />;
+    }
+
+    return primarySafe ? (
+      <MerchantContent
+        showSafePrimarySelection={safesCount > 1}
+        isPrimarySafe
+        merchantSafe={primarySafe}
+        isRefreshingBalances={isFetching}
+        refetch={refetch}
+      />
+    ) : (
+      <CreateProfile />
+    );
+  }, [primarySafe, isFetching, safesCount, refetch]);
+
+  const isMainNet = useMemo(() => network === Network.mainnet, [network]);
 
   const onRefresh = useCallback(() => {
     refetch && refetch();
@@ -36,9 +48,15 @@ const ProfileScreen = () => {
       flex={1}
     >
       <MainHeader title={strings.header.profile} />
-      <Container justifyContent="center" flexGrow={1}>
-        {!primarySafe && isFetching ? (
-          <ActivityIndicator size="large" color="white" />
+      <Container
+        justifyContent="center"
+        flexGrow={1}
+        paddingHorizontal={isMainNet ? 5 : 0}
+      >
+        {isMainNet ? (
+          <Text color="white" fontSize={26}>
+            {strings.stepOne.switchToGnosisChain}
+          </Text>
         ) : (
           ProfileBody
         )}
