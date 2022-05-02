4cfd460b4d3d2033abb59673df2837324fb5b9e2
tintinthong <justinthong93@gmail.com>
2022-05-02 20:48:51 +0800

    use to fixed

--- cardstack/src/services/merchant/merchant-service.ts
+++ cardstack/src/services/merchant/merchant-service.ts
@@ -30,7 +30,7 @@ export const claimMerchantRevenue = async ({
     // divide amount by 2 for estimate since we can't estimate the full amount
     // and the amount doesn't affect the gas price
     const claimEstimateAmount = Web3.utils.toWei(
-      new BigNumber(token.balance.amount).div(new BigNumber('2')).toString()
+      new BigNumber(token.balance.amount).div(new BigNumber('2')).toFixed()
     );
 
     const gasEstimate = await revenuePool.claimGasEstimate(
