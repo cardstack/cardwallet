dd18fa1656a58dc8e73684773202b6db19fe9145
tintinthong <justinthong93@gmail.com>
2022-05-03 18:02:46 +0800

    fix lint

--- cardstack/src/services/merchant/merchant-service.ts
+++ cardstack/src/services/merchant/merchant-service.ts
@@ -1,6 +1,4 @@
 import { getSDK } from '@cardstack/cardpay-sdk';
-import BigNumber from 'bignumber.js';
-import Web3 from 'web3';
 
 import Web3Instance from '@cardstack/models/web3-instance';
 import { TokenType } from '@cardstack/types';
