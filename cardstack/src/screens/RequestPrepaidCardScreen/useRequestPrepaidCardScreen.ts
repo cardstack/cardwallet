7e30dba9f2d07a223d46e55a8f02a2ff4b1dfcef
Paula Penedo <paulinhapenedo@gmail.com>
2022-05-20 15:37:55 -0300

    fix: remove type cast to avoid status being undefined

--- cardstack/src/screens/RequestPrepaidCardScreen/useRequestPrepaidCardScreen.ts
+++ cardstack/src/screens/RequestPrepaidCardScreen/useRequestPrepaidCardScreen.ts
@@ -1,4 +1,3 @@
-import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query/fetchBaseQuery';
 import { useCallback, useMemo, useState, useEffect } from 'react';
 import { Linking } from 'react-native';
 
@@ -38,9 +37,9 @@ export const useRequestPrepaidCardScreen = () => {
   ] = useRequestEmailCardDropMutation();
 
   const errorMessage = useMemo(() => {
-    const { status } = error as FetchBaseQueryError;
+    const is503 = error && 'status' in error && error.status === 503;
 
-    return status === 503 ? strings.customError : defaultErrorAlert;
+    return is503 ? strings.customError : defaultErrorAlert;
   }, [error]);
 
   useMutationEffects(
