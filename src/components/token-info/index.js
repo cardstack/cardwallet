d33d5847816de3394097dfaaa8b5b65f9404a07f
Paula Penedo <paulinhapenedo@gmail.com>
2022-05-26 18:44:35 -0300

    refactor: remove unused files

--- src/components/token-info/index.js
+++
@@ -1,6 +0,0 @@
-export { default as TokenInfoBalanceValue } from './TokenInfoBalanceValue';
-export { default as TokenInfoHeading } from './TokenInfoHeading';
-export { default as TokenInfoItem } from './TokenInfoItem';
-export { default as TokenInfoRow } from './TokenInfoRow';
-export { default as TokenInfoSection } from './TokenInfoSection';
-export { default as TokenInfoValue } from './TokenInfoValue';
