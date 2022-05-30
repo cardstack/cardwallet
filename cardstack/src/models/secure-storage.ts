1aaba20971ed0a73688ecee0e79b7325bd03bdc9
danibonilha <dani.bonilha@live.com>
2022-05-30 18:16:36 -0300

    [CS-3871]: Add seedPhrase secure methods

--- cardstack/src/models/secure-storage.ts
+++ cardstack/src/models/secure-storage.ts
@@ -1,6 +1,7 @@
 import * as SecureStore from 'expo-secure-store';
 import { SECURE_STORE_KEY } from 'react-native-dotenv';
 
+import AesEncryptor from '@rainbow-me/handlers/aesEncryption';
 import logger from 'logger';
 
 const keys = {
@@ -36,4 +37,58 @@ const getPin = () => getSecureValue(keys.AUTH_PIN);
 
 const deletePin = () => SecureStore.deleteItemAsync(keys.AUTH_PIN);
 
-export { savePin, getPin, deletePin };
+const encryptor = new AesEncryptor();
+
+// SEED
+const getSeedKey = (walletId: string) => `${keys.SEED}_${walletId}`;
+
+const saveSeedPhrase = async (seed: string, walletId: string) => {
+  try {
+    const pin = await getPin();
+    console.log({ pin });
+
+    if (pin) {
+      const encryptedSeed = await encryptor.encrypt(pin, seed);
+
+      if (encryptedSeed) {
+        console.log({ encryptedSeed });
+        const seedKey = getSeedKey(walletId);
+
+        await SecureStore.setItemAsync(seedKey, encryptedSeed);
+      }
+    }
+  } catch (e) {
+    logger.sentry('Error saving seed', e);
+  }
+};
+
+const getSeedPhrase = async (walletId: string, pin: string) => {
+  const seedKey = getSeedKey(walletId);
+
+  try {
+    const encryptedSeed = await getSecureValue(seedKey as Keys);
+
+    if (encryptedSeed) {
+      const seed = await encryptor.decrypt(pin, encryptedSeed);
+
+      return seed;
+    }
+  } catch (e) {
+    logger.sentry('Error retrieving seed', e);
+  }
+};
+
+const deleteSeedPhrase = async (walletId: string) => {
+  const seedKey = getSeedKey(walletId);
+
+  await SecureStore.deleteItemAsync(seedKey);
+};
+
+export {
+  savePin,
+  getPin,
+  deletePin,
+  deleteSeedPhrase,
+  getSeedPhrase,
+  saveSeedPhrase,
+};
