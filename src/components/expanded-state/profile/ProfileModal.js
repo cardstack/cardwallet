ce0c758b670a361d99f36f3973a20b6bc7f6a383
Douglas Schmidt <douglaslondrina@gmail.com>
2022-06-01 17:40:12 -0300

    Refactores ProfileModal to use centeredcontainer and to handle keyboard by itself; Removes uneded KeyboardFixedOpenLayout.

--- src/components/expanded-state/profile/ProfileModal.js
+++ src/components/expanded-state/profile/ProfileModal.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { KeyboardAvoidingView } from 'react-native';
 import TouchableBackdrop from '../../TouchableBackdrop';
 import { AssetPanel, FloatingPanels } from '../../floating-panels';
-import { KeyboardFixedOpenLayout } from '../../layout';
+
+import { CenteredContainer } from '@cardstack/components';
+import { Device } from '@cardstack/utils';
 
 import { useDimensions } from '@rainbow-me/hooks';
 
@@ -9,11 +12,13 @@ export default function ProfileModal({ onPressBackdrop, ...props }) {
   const { width: deviceWidth } = useDimensions();
 
   return (
-    <KeyboardFixedOpenLayout>
-      <TouchableBackdrop onPress={onPressBackdrop} />
-      <FloatingPanels maxWidth={deviceWidth - 110}>
-        <AssetPanel {...props} />
-      </FloatingPanels>
-    </KeyboardFixedOpenLayout>
+    <KeyboardAvoidingView behavior={Device.keyboardBehavior}>
+      <CenteredContainer width={deviceWidth}>
+        <TouchableBackdrop onPress={onPressBackdrop} />
+        <FloatingPanels maxWidth={deviceWidth - 110}>
+          <AssetPanel {...props} />
+        </FloatingPanels>
+      </CenteredContainer>
+    </KeyboardAvoidingView>
   );
 }
