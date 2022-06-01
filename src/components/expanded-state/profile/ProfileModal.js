860cffc80747a1dfd2ca7700353734b4f8f88f12
Douglas Schmidt <douglaslondrina@gmail.com>
2022-06-01 08:37:33 -0300

    Simplifies ProfileModal and KeyboardFixedOpenLayout.

--- src/components/expanded-state/profile/ProfileModal.js
+++ src/components/expanded-state/profile/ProfileModal.js
@@ -1,18 +1,15 @@
-import { useRoute } from '@react-navigation/native';
 import React from 'react';
 import TouchableBackdrop from '../../TouchableBackdrop';
 import { AssetPanel, FloatingPanels } from '../../floating-panels';
 import { KeyboardFixedOpenLayout } from '../../layout';
+
 import { useDimensions } from '@rainbow-me/hooks';
 
 export default function ProfileModal({ onPressBackdrop, ...props }) {
   const { width: deviceWidth } = useDimensions();
-  const { params } = useRoute();
 
   return (
-    <KeyboardFixedOpenLayout
-      additionalPadding={params?.additionalPadding ? 80 : 0}
-    >
+    <KeyboardFixedOpenLayout>
       <TouchableBackdrop onPress={onPressBackdrop} />
       <FloatingPanels maxWidth={deviceWidth - 110}>
         <AssetPanel {...props} />
--- src/components/layout/KeyboardFixedOpenLayout.js
+++ src/components/layout/KeyboardFixedOpenLayout.js
@@ -1,48 +1,18 @@
-import React, { useEffect, useRef } from 'react';
-import { KeyboardAvoidingView } from 'react-native';
-import { Transition, Transitioning } from 'react-native-reanimated';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import React from 'react';
+import { KeyboardAvoidingView, View } from 'react-native';
 import styled from 'styled-components';
-import { useDimensions, useKeyboardHeight } from '../../hooks';
 import Centered from './Centered';
-import { position } from '@rainbow-me/styles';
 
-const Container = styled(Transitioning.View)`
-  height: ${({ height }) => height};
-  left: 0;
-  position: ${android ? 'relative' : 'absolute'};
-  right: 0;
-  top: 0;
+const Container = styled(View)`
+  height: 100%;
+  width: 100%;
 `;
 
-const InnerWrapper = styled(Centered)`
-  ${position.size('100%')};
-  padding-bottom: 10;
-  padding-top: ${({ insets }) => insets.top};
-`;
-
-const transition = (
-  <Transition.Change durationMs={150} interpolation="easeOut" />
-);
-
-export default function KeyboardFixedOpenLayout({
-  additionalPadding = 0,
-  ...props
-}) {
-  const insets = useSafeAreaInsets();
-  const { height: screenHeight } = useDimensions();
-  const keyboardHeight = useKeyboardHeight();
-  const ref = useRef();
-
-  const containerHeight = screenHeight - keyboardHeight - additionalPadding;
-  ios &&
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => ref.current.animateNextTransition(), [containerHeight]);
-
+export default function KeyboardFixedOpenLayout({ ...props }) {
   return (
-    <Container height={containerHeight} ref={ref} transition={transition}>
-      <KeyboardAvoidingView behavior="height" enabled={!keyboardHeight}>
-        <InnerWrapper {...props} insets={insets} />
+    <Container>
+      <KeyboardAvoidingView behavior="padding">
+        <Centered {...props} />
       </KeyboardAvoidingView>
     </Container>
   );
