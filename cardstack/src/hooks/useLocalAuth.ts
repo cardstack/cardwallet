590771b63f797dd2469b1c4d06633923c4818f5e
Douglas Schmidt <douglaslondrina@gmail.com>
2022-06-03 11:25:30 -0300

    Removes unused import

--- cardstack/src/hooks/useLocalAuth.ts
+++ cardstack/src/hooks/useLocalAuth.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import * as BiometricAuth from '@cardstack/models/biometric-auth';
