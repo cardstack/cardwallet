5e96d432880fd032a36af166c328c41bfe3961cc
oidoug <github@oidoug.com>
2022-06-06 10:40:55 -0300

    WIP: Local Auth

--- cardstack/src/models/biometric-auth.ts
+++ cardstack/src/models/biometric-auth.ts
@@ -1,4 +1,6 @@
 import * as LocalAuthentication from 'expo-local-authentication';
+export { SecurityLevel, AuthenticationType } from 'expo-local-authentication';
+import { last } from 'lodash';
 
 import logger from 'logger';
 
@@ -16,4 +18,29 @@ const authenticate = async (
   }
 };
 
-export { authenticate };
+const securityLevel = async () => {
+  try {
+    return await LocalAuthentication.getEnrolledLevelAsync();
+  } catch (error) {
+    logger.sentry('Local authentication failed', error);
+
+    return false;
+  }
+};
+
+const authenticationType = async () => {
+  try {
+    const response = await LocalAuthentication.supportedAuthenticationTypesAsync();
+
+    return last(response);
+  } catch (error) {
+    logger.sentry('Local authentication failed', error);
+
+    return false;
+  }
+};
+
+// todo: Check hardware support
+// todo: Check security level for icons
+
+export { authenticate, securityLevel, authenticationType };
