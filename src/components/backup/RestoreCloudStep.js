c34d9b05a0a6a6350f866f3be1e7067e183078e9
danibonilha <dani.bonilha@live.com>
2022-07-11 15:14:18 -0300

    [CS-4161]: Simpfly backup to be retrocompatible

--- src/components/backup/RestoreCloudStep.js
+++ src/components/backup/RestoreCloudStep.js
@@ -1,10 +1,6 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Alert } from 'react-native';
-import {
-  fetchBackupPassword,
-  restoreCloudBackup,
-  saveBackupPassword,
-} from '../../model/backup';
+import { restoreCloudBackup } from '../../../cardstack/src/models/backup';
 import BackupSheetKeyboardLayout from './BackupSheetKeyboardLayout';
 import { Button, Container, Icon, Input, Text } from '@cardstack/components';
 import {
@@ -16,33 +12,18 @@ import {
   cloudBackupPasswordMinLength,
   isCloudBackupPasswordValid,
 } from '@rainbow-me/handlers/cloudBackup';
-import { removeWalletData } from '@rainbow-me/handlers/localstorage/removeWallet';
 import WalletLoadingStates from '@rainbow-me/helpers/walletLoadingStates';
-import { useAccountSettings, useWalletManager } from '@rainbow-me/hooks';
+import logger from 'logger';
 
-export default function RestoreCloudStep({
-  userData,
-  backupSelected,
-  fromSettings,
-}) {
+export default function RestoreCloudStep({ userData, backupSelected }) {
   const selectedBackupName = backupSelected?.name;
   const [validPassword, setValidPassword] = useState(false);
   const [incorrectPassword, setIncorrectPassword] = useState(false);
   const [password, setPassword] = useState('');
   const [label, setLabel] = useState('Restore from backup');
   const passwordRef = useRef();
-  const { accountAddress } = useAccountSettings();
-  const { initWalletResetNavState } = useWalletManager();
 
-  useEffect(() => {
-    const fetchPasswordIfPossible = async () => {
-      const pwd = await fetchBackupPassword();
-      if (pwd) {
-        setPassword(pwd);
-      }
-    };
-    fetchPasswordIfPossible();
-  }, []);
+  const { showLoadingOverlay, dismissLoadingOverlay } = useLoadingOverlay();
 
   useEffect(() => {
     let newLabel = '';
@@ -70,42 +51,23 @@ export default function RestoreCloudStep({
     []
   );
 
-  const { showLoadingOverlay, dismissLoadingOverlay } = useLoadingOverlay();
-
   const onSubmit = useCallback(async () => {
     try {
       dismissKeyboardOnAndroid();
 
       showLoadingOverlay({ title: WalletLoadingStates.RESTORING_WALLET });
-      const success = await restoreCloudBackup(
-        password,
-        userData,
-        selectedBackupName
-      );
 
-      if (success) {
-        // Store it in the keychain in case it was missing
-        await saveBackupPassword(password);
-
-        if (accountAddress) {
-          // Get rid of the current wallet
-          await removeWalletData(accountAddress);
-        }
-
-        await initWalletResetNavState(undefined, fromSettings);
-      } else {
-        setIncorrectPassword(true);
-        dismissLoadingOverlay();
-      }
+      await restoreCloudBackup(password, userData, selectedBackupName);
     } catch (e) {
-      dismissLoadingOverlay();
+      setIncorrectPassword(true);
+
+      logger.sentry('Error while restoring backup', e);
       Alert.alert('Error while restoring backup');
+    } finally {
+      dismissLoadingOverlay();
     }
   }, [
-    accountAddress,
     dismissLoadingOverlay,
-    fromSettings,
-    initWalletResetNavState,
     password,
     selectedBackupName,
     showLoadingOverlay,
--- src/model/backup.ts
+++ src/model/backup.ts
@@ -1,35 +1,20 @@
 import assert from 'assert';
 import { captureException } from '@sentry/react-native';
-import { endsWith, forEach, map } from 'lodash';
-import { ACCESSIBLE, Options } from 'react-native-keychain';
+import { forEach, startsWith } from 'lodash';
 import {
-  CLOUD_BACKUP_ERRORS,
   encryptAndSaveDataToCloud,
   getDataFromCloud,
 } from '../handlers/cloudBackup';
 import WalletBackupTypes from '../helpers/walletBackupTypes';
-import WalletTypes from '../helpers/walletTypes';
-import {
-  allWalletsKey,
-  iCloudKey,
-  pinKey,
-  privateKeyKey,
-  seedPhraseKey,
-  selectedWalletKey,
-} from '../utils/keychainConstants';
-import * as keychain from './keychain';
+
 import {
   AllRainbowWallets,
-  allWalletsVersion,
   createOrImportWallet,
-  publicAccessControlOptions,
   RainbowWallet,
 } from './wallet';
 
-import { Device } from '@cardstack/utils/device';
+import { getSeedPhrase } from '@cardstack/models/secure-storage';
 import logger from 'logger';
-
-type BackupPassword = string;
 
 interface BackedUpData {
   [key: string]:
@@ -47,113 +32,44 @@ interface BackupUserData {
 export interface ICloudBackupData {
   createdAt: number;
   updatedAt: number;
-  secrets: { [key: string]: string };
+  secrets?: { [key: string]: string };
+  seedPhrase?: string;
 }
 
-async function extractSecretsForWallet(wallet: RainbowWallet) {
-  const allKeys = await keychain.loadAllKeys();
-  if (!allKeys) throw new Error(CLOUD_BACKUP_ERRORS.KEYCHAIN_ACCESS_ERROR);
-  const secrets = {} as { [key: string]: string };
-
-  const allowedPkeysKeys = map(
-    wallet?.addresses,
-    account => `${account.address}_${privateKeyKey}`
-  );
-
-  allKeys.forEach(item => {
-    // Ignore allWalletsKey
-    if (item.username === allWalletsKey) {
-      return;
-    }
-
-    // Ignore selected wallet
-    if (item.username === selectedWalletKey) {
-      return;
-    }
-
-    // Ignore another wallets seeds
-    if (
-      item.username.indexOf(`_${seedPhraseKey}`) !== -1 &&
-      item.username !== `${wallet.id}_${seedPhraseKey}`
-    ) {
-      return;
-    }
-
-    // Ignore other wallets PKeys
-    if (
-      item.username.indexOf(`_${privateKeyKey}`) !== -1 &&
-      allowedPkeysKeys.indexOf(item.username) === -1
-    ) {
-      return;
-    }
-
-    // Ignore pinKey
-    if (item.username === pinKey) {
-      return;
-    }
-
-    secrets[item.username] = item.password;
-  });
-  return secrets;
-}
+const isBackedUpWallet = (wallet: RainbowWallet) =>
+  wallet.backedUp &&
+  wallet.backupDate &&
+  wallet.backupFile &&
+  typeof wallet.backupFile === 'string' &&
+  wallet.backupType === WalletBackupTypes.cloud;
 
 export async function backupWalletToCloud(
-  password: BackupPassword,
+  password: string,
   wallet: RainbowWallet
 ) {
   const now = Date.now();
 
-  logger.log('calling extractSecretsForWallet');
-  const secrets = await extractSecretsForWallet(wallet);
+  const seedPhrase = await getSeedPhrase(wallet.id);
+
   const data = {
     createdAt: now,
-    secrets,
-  } as ICloudBackupData;
+    seedPhrase,
+  };
+
   logger.log('calling encryptAndSaveDataToCloud');
   return encryptAndSaveDataToCloud(data, password, `backup_${now}.json`);
 }
 
-export async function addWalletToCloudBackup(
-  password: BackupPassword,
-  wallet: RainbowWallet,
-  filename: string
-) {
-  const backup = await getDataFromCloud(password, filename);
-  assert(backup, 'No backup found');
-  const now = Date.now();
-
-  const secrets = await extractSecretsForWallet(wallet);
-
-  backup.updatedAt = now;
-  // Merge existing secrets with the ones from this wallet
-  backup.secrets = {
-    ...backup.secrets,
-    ...secrets,
-  };
-  const savedFilename = await encryptAndSaveDataToCloud(
-    backup,
-    password,
-    filename
-  );
-
-  return savedFilename;
-}
-
-export function findLatestBackUp(wallets: AllRainbowWallets): string | null {
-  let latestBackup: string | null = null;
-  let filename: string | null = null;
+export function findLatestBackUp(wallets: AllRainbowWallets) {
+  let latestBackup: string | undefined;
+  let filename: string | undefined;
 
   forEach(wallets, wallet => {
     // Check if there's a wallet backed up
-    if (
-      wallet.backedUp &&
-      wallet.backupDate &&
-      wallet.backupFile &&
-      typeof wallet.backupFile === 'string' &&
-      wallet.backupType === WalletBackupTypes.cloud
-    ) {
+    if (isBackedUpWallet(wallet)) {
       // If there is one, let's grab the latest backup
-      if (!latestBackup || wallet.backupDate > latestBackup) {
+      // @ts-expect-error isBackupWallet checks undefined values
+      if (!latestBackup || wallet?.backupDate > latestBackup) {
         filename = wallet.backupFile;
         latestBackup = wallet.backupDate;
       }
@@ -164,146 +80,69 @@ export function findLatestBackUp(wallets: AllRainbowWallets): string | null {
 }
 
 export async function restoreCloudBackup(
-  password: BackupPassword,
+  password: string,
   userData: BackupUserData | null,
   backupSelected: string | null
-): Promise<boolean> {
-  // We support two flows
-  // Restoring from the welcome screen, which uses the userData to rebuild the wallet
-  // Restoring a specific backup from settings => Backup, which uses only the keys stored.
-
+) {
   try {
     const filename =
       backupSelected || (userData && findLatestBackUp(userData?.wallets));
+
     if (!filename) {
       return false;
     }
-    // 2- download that backup
+
     const data = await getDataFromCloud(password, filename);
+
     if (!data) {
       throw new Error('Invalid password');
     }
-    let dataToRestore = {
-      ...data.secrets,
-    } as {
-      [key: string]: string | { version: number; wallets: AllRainbowWallets };
-    };
 
     if (userData) {
-      // Restore only wallets that were backed up in cloud
-      // or wallets that are read-only
-      const walletsToRestore: AllRainbowWallets = {};
-      forEach(userData.wallets, wallet => {
-        if (
-          (wallet.backedUp &&
-            wallet.backupDate &&
-            wallet.backupFile &&
-            typeof wallet.backupFile === 'string' &&
-            wallet.backupType === WalletBackupTypes.cloud) ||
-          wallet.type === WalletTypes.readOnly
-        ) {
-          walletsToRestore[wallet.id] = wallet;
-        }
-      });
+      const firstEligibleWallet = Object.values(userData.wallets).find(
+        isBackedUpWallet
+      );
 
-      // All wallets
-      dataToRestore[allWalletsKey] = {
-        version: allWalletsVersion,
-        wallets: walletsToRestore,
-      };
-      return restoreCurrentBackupIntoKeychain(dataToRestore);
-    } else {
-      return restoreSpecificBackupIntoKeychain(dataToRestore);
+      const seed =
+        data.seedPhrase ||
+        (await findAndParseOldSeed(data.secrets, firstEligibleWallet?.id));
+
+      await createOrImportWallet({ seed });
     }
   } catch (e) {
     logger.sentry('Error while restoring back up');
     captureException(e);
-    return false;
   }
 }
 
-async function restoreSpecificBackupIntoKeychain(
-  backedUpData: BackedUpData
-): Promise<boolean> {
-  try {
-    // Re-import all the seeds (and / or pkeys) one by one
-    for (const key of Object.keys(backedUpData)) {
-      if (endsWith(key, seedPhraseKey)) {
-        const valueStr = backedUpData[key];
-        assert(typeof valueStr == 'string', 'Seed is not a string');
-        const { seedphrase } = JSON.parse(valueStr);
-        await createOrImportWallet({ seed: seedphrase });
-      }
-    }
-    return true;
-  } catch (e) {
-    logger.sentry('error in restoreSpecificBackupIntoKeychain');
-    captureException(e);
-    return false;
+async function findAndParseOldSeed(
+  backedUpData?: BackedUpData,
+  walletId?: string
+) {
+  if (!backedUpData || walletId) {
+    logger.sentry('no backupData or walletId found');
+    return;
   }
-}
 
-async function restoreCurrentBackupIntoKeychain(
-  backedUpData: BackedUpData
-): Promise<boolean> {
-  try {
-    // Access control config per each type of key
-    const privateAccessControlOptions = await keychain.getPrivateAccessControlOptions();
+  const backupKey = Object.keys(backedUpData).find(key =>
+    startsWith(key, `${walletId}_`)
+  );
 
-    await Promise.all(
-      Object.keys(backedUpData).map(async key => {
-        const value = backedUpData[key];
-        let accessControl: Options = publicAccessControlOptions;
-        if (endsWith(key, seedPhraseKey) || endsWith(key, privateKeyKey)) {
-          accessControl = privateAccessControlOptions;
-        }
-        if (typeof value === 'string') {
-          return keychain.saveString(key, value, accessControl);
-        } else {
-          return keychain.saveObject(key, value, accessControl);
-        }
-      })
-    );
-
-    return true;
-  } catch (e) {
-    logger.sentry('error in restoreBackupIntoKeychain');
-    captureException(e);
-    return false;
-  }
-}
-
-// Attempts to save the password to decrypt the backup from the iCloud keychain
-export async function saveBackupPassword(
-  password: BackupPassword
-): Promise<void> {
-  try {
-    if (Device.isIOS) {
-      await keychain.saveString(iCloudKey, password, {
-        accessible: ACCESSIBLE.ALWAYS_THIS_DEVICE_ONLY,
-      });
-    }
-  } catch (e) {
-    logger.sentry('Error while backing up password', e);
-    captureException(e);
-  }
-}
-
-// Attempts to fetch the password to decrypt the backup from the iCloud keychain
-export async function fetchBackupPassword(): Promise<string | null> {
-  if (Device.isAndroid) {
-    return null;
+  if (!backupKey) {
+    logger.sentry('no backupKeyfound');
+    return;
   }
 
   try {
-    const password = (await keychain.loadString(iCloudKey)) || null;
-    if (typeof password === 'string') {
-      return password;
-    }
-    throw new Error('Unexpected response loading decryption password');
+    const valueStr = backedUpData[backupKey];
+
+    assert(typeof valueStr == 'string', 'Seed is not a string');
+
+    const { seedphrase } = JSON.parse(valueStr);
+
+    return seedphrase;
   } catch (e) {
-    logger.sentry('Error while fetching backup password', e);
+    logger.sentry('error in findAndParseOldSeed');
     captureException(e);
-    return null;
   }
 }
