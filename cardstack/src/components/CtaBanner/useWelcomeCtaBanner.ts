6ec7e5cf1600a1b4f25906b9a8b5d139736cec65
Paula Penedo <paulinhapenedo@gmail.com>
2022-07-11 16:24:48 -0300

    feat: adds polling to the EoaClaimedQuery

--- cardstack/src/components/CtaBanner/useWelcomeCtaBanner.ts
+++ cardstack/src/components/CtaBanner/useWelcomeCtaBanner.ts
@@ -1,12 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { Routes } from '@cardstack/navigation';
 import { useGetEoaClaimedQuery } from '@cardstack/services/hub/hub-api';
 import { remoteFlags } from '@cardstack/services/remote-config';
-import { isLayer2 } from '@cardstack/utils';
+import { isLayer2, PREPAID_CARD_CLAIMED_KEY } from '@cardstack/utils';
 
 import { useWallets, useAccountSettings } from '@rainbow-me/hooks';
+import { logger } from '@rainbow-me/utils';
 
 import { useCtaBanner } from '.';
 
@@ -15,14 +17,14 @@ const WELCOME_BANNER_KEY = 'WELCOME_BANNER_KEY';
 export const useWelcomeCtaBanner = () => {
   const { selectedAccount } = useWallets();
   const { accountAddress, network } = useAccountSettings();
+  const [hasClaimed, setHasClaimed] = useState(true);
 
-  const {
-    data: emailDropGetData = { showBanner: false },
-  } = useGetEoaClaimedQuery(
+  const { data: emailDropGetData } = useGetEoaClaimedQuery(
+    { eoa: accountAddress },
     {
-      eoa: accountAddress,
-    },
-    { skip: !accountAddress }
+      skip: !accountAddress,
+      pollingInterval: hasClaimed ? undefined : 5000,
+    }
   );
 
   const { showBanner: showBannerUserDecision, dismissBanner } = useCtaBanner(
@@ -48,7 +50,7 @@ export const useWelcomeCtaBanner = () => {
       remoteFlags().featurePrepaidCardDrop &&
       showBannerUserDecision &&
       isFirstAddressForCurrentWallet &&
-      emailDropGetData?.showBanner,
+      (emailDropGetData?.showBanner ?? false),
     [
       showBannerUserDecision,
       isFirstAddressForCurrentWallet,
@@ -56,6 +58,36 @@ export const useWelcomeCtaBanner = () => {
       emailDropGetData,
     ]
   );
+
+  const handleClaimStatus = useCallback(async () => {
+    try {
+      if (emailDropGetData?.claimed) {
+        await AsyncStorage.setItem(PREPAID_CARD_CLAIMED_KEY, accountAddress);
+        setHasClaimed(true);
+
+        return;
+      }
+
+      const claimedKey = await AsyncStorage.getItem(PREPAID_CARD_CLAIMED_KEY);
+
+      if (
+        !claimedKey ||
+        claimedKey !== accountAddress ||
+        !emailDropGetData?.claimed
+      ) {
+        setHasClaimed(false);
+      }
+    } catch (error) {
+      logger.log('[Async Storage] Error getting key', error);
+    }
+  }, [accountAddress, emailDropGetData]);
+
+  // handles/enables polling only if banner is visible
+  useEffect(() => {
+    if (showBanner) {
+      handleClaimStatus();
+    }
+  }, [handleClaimStatus, showBanner]);
 
   return {
     showBanner,
