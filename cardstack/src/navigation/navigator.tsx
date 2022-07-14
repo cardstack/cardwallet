6053226c10f1d3c9cff8e86a12152f81f3e91f77
Paula Penedo <paulinhapenedo@gmail.com>
2022-07-14 17:10:30 -0300

    feat: pass the linking config only when the user is authorized

--- cardstack/src/navigation/navigator.tsx
+++ cardstack/src/navigation/navigator.tsx
@@ -1,20 +1,28 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React, { memo } from 'react';
 
+import { useAuthSelectorAndActions } from '@cardstack/redux/authSlice';
+
 import { navigationRef } from './Navigation';
 import { onNavigationStateChange } from './onNavigationStateChange';
 import { linking } from './screens';
 import { StackNavigator } from './tabBarNavigator';
 
-const AppContainer = () => (
-  <NavigationContainer
-    linking={linking}
-    onStateChange={onNavigationStateChange}
-    ref={navigationRef}
-  >
-    <StackNavigator />
-  </NavigationContainer>
-);
+const AppContainer = () => {
+  const { isAuthorized } = useAuthSelectorAndActions();
+
+  const hasLinking = isAuthorized ? linking : undefined;
+
+  return (
+    <NavigationContainer
+      linking={hasLinking}
+      onStateChange={onNavigationStateChange}
+      ref={navigationRef}
+    >
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
 
 AppContainer.displayName = 'AppContainer';
 
