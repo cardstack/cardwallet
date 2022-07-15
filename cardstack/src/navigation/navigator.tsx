dd349a1a58164bdeefe6ecfcafcb4b099b611157
Paula Penedo <paulinhapenedo@gmail.com>
2022-07-15 10:20:36 -0300

    [CS-4237] Handle deeplink when user dismiss biometric authentication once (#943)
    * feat: pass the linking config only when the user is authorized
    
    * feat: save deeplink when app is on background unauthorized
    
    * rename variable
    
    * create handleDeepLink wrapper

--- cardstack/src/navigation/navigator.tsx
+++ cardstack/src/navigation/navigator.tsx
@@ -1,20 +1,28 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React, { memo } from 'react';
 
+import { useAuthSelector } from '@cardstack/redux/authSlice';
+
 import { navigationRef } from './Navigation';
 import { onNavigationStateChange } from './onNavigationStateChange';
 import { linking } from './screens';
 import { StackNavigator } from './tabBarNavigator';
 
-const AppContainer = () => (
-  <NavigationContainer
-    linking={linking}
-    onStateChange={onNavigationStateChange}
-    ref={navigationRef}
-  >
-    <StackNavigator />
-  </NavigationContainer>
-);
+const AppContainer = () => {
+  const { isAuthorized } = useAuthSelector();
+
+  const enableLinking = isAuthorized ? linking : undefined;
+
+  return (
+    <NavigationContainer
+      linking={enableLinking}
+      onStateChange={onNavigationStateChange}
+      ref={navigationRef}
+    >
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
 
 AppContainer.displayName = 'AppContainer';
 
