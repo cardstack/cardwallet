f518f5392ca27879dedcb3752015b1a46fa95fc3
Douglas Schmidt <github@oidoug.com>
2022-07-19 15:15:30 -0300

    Comprehensive testing for purchase profile hook.

--- cardstack/src/hooks/__tests__/usePurchaseProfile.test.ts
+++ cardstack/src/hooks/__tests__/usePurchaseProfile.test.ts
@@ -1,10 +1,11 @@
 import { renderHook } from '@testing-library/react-hooks';
-import { waitFor } from '@testing-library/react-native';
+import { act } from '@testing-library/react-native';
 import { useIAP, Product, Purchase } from 'react-native-iap';
 
 // import { CreateBusinessInfoDIDParams } from '@cardstack/types';
 
 import { useAccountProfile } from '@rainbow-me/hooks';
+import logger from 'logger';
 
 import { usePurchaseProfile } from '../usePurchaseProfile';
 
@@ -32,32 +33,32 @@ jest.mock('@rainbow-me/hooks', () => ({
   useAccountProfile: jest.fn(),
 }));
 
-const mockedRequestPurchase = jest.fn().mockResolvedValue(true);
 jest.mock('react-native-iap', () => ({
   useIAP: jest.fn(),
-  requestPurchase: mockedRequestPurchase,
-}));
-
-const mockedLog = jest.fn().mockImplementation();
-const mockedSentry = jest.fn().mockImplementation();
-jest.mock('logger', () => ({
-  log: mockedLog,
-  sentry: mockedSentry,
+  // requestPurchase: mockedRequestPurchase,
 }));
 
 describe('usePurchaseProfile', () => {
+  const mockedGetProducts = jest.fn();
+  const mockedGetPurchases = jest.fn();
+  const mockedRequestPurchase = jest.fn();
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   beforeEach(() => {
+    logger.sentry = jest.fn();
+    logger.log = jest.fn();
+
     (useAccountProfile as jest.Mock).mockImplementation(
       () => mockAccountProfile
     );
 
     (useIAP as jest.Mock).mockImplementation(() => ({
-      getProducts: jest.fn().mockResolvedValue(mockProduct),
-      getAvailablePurchases: jest.fn().mockResolvedValue(mockPurchase),
+      getProducts: mockedGetProducts.mockResolvedValue(mockProduct),
+      getAvailablePurchases: mockedGetPurchases.mockResolvedValue(mockPurchase),
+      requestPurchase: mockedRequestPurchase.mockResolvedValue(null),
       products: [mockProduct],
     }));
   });
@@ -82,85 +83,74 @@ describe('usePurchaseProfile', () => {
     expect(result.current.products).toMatchObject([mockProduct]);
   });
 
-  it('should log a response from getProducts call', () => {
-    renderHook(() => usePurchaseProfile());
+  it('should request purchase when product purchase called', async () => {
+    const { result, waitFor } = renderHook(() => usePurchaseProfile());
 
-    waitFor(() => {
-      expect(mockedLog).toBeCalledWith('wrong output');
-      // expect(mockedLog).toHaveBeenLastCalledWith(`[IAP] Products response {
-      //   type: 'iap',
-      //   productId: '0001',
-      //   title: 'Mock',
-      //   description: 'Mock',
-      //   price: '0.99',
-      //   currency: 'USD',
-      //   localizedPrice: '0.99'
-      // }`);
+    await waitFor(() => {
+      expect(mockedGetProducts).toHaveReturned();
     });
-  });
 
-  // it('should log a response from getProducts call', () => {
-  //   jest.spyOn(logger, 'log').mockImplementation();
-
-  //   renderHook(() => usePurchaseProfile());
-
-  //   waitFor(() => {
-  //     expect(logger.log).toBeCalledWith('wrong output');
-  //   });
-  // });
-
-  /* Following tests always pass, is not asserting the correct logged output.
-   *
-  it('should log a response from getAvailablePurchases call', () => {
-    renderHook(() => usePurchaseProfile());
-
-    waitFor(() => {
-      expect(mockedLog)
-        .toHaveBeenLastCalledWith(`[IAP] Available Purchases response {
-          productId: '0001',
-          transactionDate: 10101010,
-          transactionReceipt: 'HASH_RECEIPT'
-        }`);
-    });
-  });
-
-  it('should log error on sentry if product fetching fails', () => {
-    (useIAP as jest.Mock).mockImplementation(() => ({
-      getProducts: jest.fn().mockRejectedValueOnce('Error fetching products'),
-    }));
-
-    renderHook(() => usePurchaseProfile());
-
-    waitFor(() => {
-      expect(mockedSentry).toHaveBeenLastCalledWith('Error fetching products');
-    });
-  });
-
-  it('should log error on sentry if purchases fetching fails', () => {
-    (useIAP as jest.Mock).mockImplementation(() => ({
-      getAvailablePurchases: jest
-        .fn()
-        .mockRejectedValueOnce('Error fetching purchases'),
-    }));
-
-    renderHook(() => usePurchaseProfile());
-
-    waitFor(() => {
-      expect(mockedSentry).toHaveBeenLastCalledWith('Error fetching purchases');
-    });
-  });
-  */
-
-  /* This one is looping, but can't find why...
-   *
-  it('should request purchase when product purchase called', () => {
-    const { result } = renderHook(() => usePurchaseProfile());
-
-    act(() => {
+    await act(() => {
       result.current.purchaseProduct(mockProduct);
     });
 
-    expect(mockedRequestPurchase).toBeCalledWith(mockProduct.productId);
+    await waitFor(() => {
+      expect(mockedRequestPurchase).toBeCalledWith(mockProduct.productId);
+    });
   });
-  */
+
+  it('should log a response from getProducts call', async () => {
+    const { waitFor } = renderHook(() => usePurchaseProfile());
+
+    await waitFor(() => {
+      expect(mockedGetProducts).toHaveReturned();
+      expect(logger.log).toBeCalledWith('[IAP] Products response', mockProduct);
+    });
+  });
+
+  it('should log a response from getAvailablePurchases call', async () => {
+    const { waitFor } = renderHook(() => usePurchaseProfile());
+
+    await waitFor(() => {
+      expect(mockedGetPurchases).toHaveReturned();
+      expect(logger.log).toBeCalledWith(
+        '[IAP] Available Purchases response',
+        mockPurchase
+      );
+    });
+  });
+
+  it('should log error on sentry if product fetching fails', async () => {
+    (useIAP as jest.Mock).mockImplementation(() => ({
+      getProducts: mockedGetProducts.mockRejectedValueOnce('MOCK_ERROR'),
+    }));
+
+    const { waitFor } = renderHook(() => usePurchaseProfile());
+
+    await waitFor(() => {
+      expect(mockedGetProducts).toHaveReturned();
+      expect(logger.sentry).toBeCalledWith(
+        'Error fetching products:',
+        'MOCK_ERROR'
+      );
+    });
+  });
+
+  it('should log error on sentry if purchases fetching fails', async () => {
+    (useIAP as jest.Mock).mockImplementation(() => ({
+      getAvailablePurchases: mockedGetPurchases.mockRejectedValueOnce(
+        'MOCK_ERROR'
+      ),
+    }));
+
+    const { waitFor } = renderHook(() => usePurchaseProfile());
+
+    await waitFor(() => {
+      expect(mockedGetPurchases).toHaveReturned();
+      expect(logger.sentry).toBeCalledWith(
+        'Error fetching available purchases:',
+        'MOCK_ERROR'
+      );
+    });
+  });
 });
