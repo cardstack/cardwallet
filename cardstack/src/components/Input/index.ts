9ac1d576f4153fba2783e9d8d5a69aa77ceca4af
Douglas Schmidt <github@oidoug.com>
2022-07-20 17:25:41 -0300

    Removing global export for SuffixedInput because it made some tests fail.

--- cardstack/src/components/Input/index.ts
+++ cardstack/src/components/Input/index.ts
@@ -3,4 +3,3 @@ export * from './InputAmount/InputAmount';
 export * from './InputAmount/useInputAmountHelper';
 export { default as FormInput } from './FormInput';
 export { default as PinInput } from './PinInput/PinInput';
-export { default as SuffixedInput } from './SuffixedInput/SuffixedInput';
