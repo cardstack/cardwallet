ade39ebd2f5c73c47de8ba7b34f13d4c51c93452
Paula Penedo <paulinhapenedo@gmail.com>
2022-07-28 14:01:50 -0300

    [CS-4080] In-app purchase charge explanation (#960)
    * feat: create ProfileChargeExplanationScreen
    
    * feat: connect ChargeExplanation screen with CTA screen
    
    * fix: move fee to be a param
    
    * fix: wrap component in memo
    
    * fix: move iconProps outside of react component

---
+++ cardstack/src/screens/Profile/ProfileChargeExplanationScreen/ProfileChargeExplanationScreen.tsx
@@ -0,0 +1,38 @@
+import React, { memo } from 'react';
+
+import {
+  Container,
+  IconProps,
+  InPageHeader,
+  SafeAreaView,
+  Text,
+} from '@cardstack/components';
+
+import { strings } from './strings';
+
+const leftIconProps: IconProps = { name: 'x', left: -6 };
+
+// TODO: replace this with the value from usePurchase hook
+const LOCALIZED_PRICE = '$0.99 USD';
+
+const ProfileChargeExplanationScreen = () => (
+  <SafeAreaView
+    backgroundColor="backgroundDarkPurple"
+    flex={1}
+    paddingHorizontal={5}
+    justifyContent="space-between"
+  >
+    <InPageHeader showSkipButton={false} leftIconProps={leftIconProps} />
+
+    <Container flex={0.95}>
+      <Text color="white" variant="pageHeader" paddingBottom={5}>
+        {strings.title}
+      </Text>
+      <Text color="grayText" letterSpacing={0.4}>
+        {strings.description(LOCALIZED_PRICE)}
+      </Text>
+    </Container>
+  </SafeAreaView>
+);
+
+export default memo(ProfileChargeExplanationScreen);
---
+++ cardstack/src/screens/Profile/ProfileChargeExplanationScreen/strings.ts
@@ -0,0 +1,5 @@
+export const strings = {
+  title: 'Why is there a charge?',
+  description: (fee: string) =>
+    `Profiles are on-chain. We charge a one-time ${fee} setup fee to offset gas and other administrative costs.`,
+};
