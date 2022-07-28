6afab61872e1bc0e4b755c243278d40c4ed27db6
Paula Penedo <paulinhapenedo@gmail.com>
2022-07-28 13:45:58 -0300

    fix: wrap component in memo

--- cardstack/src/screens/Profile/ProfileChargeExplanationScreen/ProfileChargeExplanationScreen.tsx
+++ cardstack/src/screens/Profile/ProfileChargeExplanationScreen/ProfileChargeExplanationScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import {
   Container,
@@ -35,4 +35,4 @@ const ProfileChargeExplanationScreen = () => (
   </SafeAreaView>
 );
 
-export default ProfileChargeExplanationScreen;
+export default memo(ProfileChargeExplanationScreen);
