efc07ac6eae9d7685aa17e0068529dbfbe067940
Douglas Schmidt <github@oidoug.com>
2022-07-29 15:05:18 -0300

    Cleans up comments.

--- cardstack/src/hooks/onboarding/useShowOnboarding.ts
+++ cardstack/src/hooks/onboarding/useShowOnboarding.ts
@@ -1,17 +1,12 @@
 import { useNavigation } from '@react-navigation/native';
-import { useCallback, useEffect, useRef, useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 
 import { Routes } from '@cardstack/navigation';
 import { usePrimarySafe } from '@cardstack/redux/hooks/usePrimarySafe';
 
-import { useWalletManager, useWallets } from '@rainbow-me/hooks';
-
 export const useShowOnboarding = () => {
   const { navigate } = useNavigation();
   const { primarySafe, isFetching } = usePrimarySafe();
-  // const { initializeWallet } = useWalletManager();
-  // const { walletReady } = useWallets();
-  // const initialized = useRef(false);
 
   const isOnboardingNeeded = useMemo(() => !isFetching && !primarySafe, [
     isFetching,
@@ -23,13 +18,6 @@ export const useShowOnboarding = () => {
       navigate(Routes.PROFILE_SLUG);
     }
   }, [isOnboardingNeeded, navigate]);
-
-  // useEffect(() => {
-  //   if (!initialized.current && !walletReady) {
-  //     initializeWallet();
-  //     initialized.current = true;
-  //   }
-  // }, [initializeWallet, walletReady]);
 
   return {
     isOnboardingNeeded,
