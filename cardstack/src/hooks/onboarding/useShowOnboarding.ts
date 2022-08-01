9105efcbcdd81587bad3e17045837211e4c89c26
Douglas Schmidt <github@oidoug.com>
2022-08-01 13:33:11 -0300

    Adds comments to why shouldPresentOnboarding is a callback and not memo.

--- cardstack/src/hooks/onboarding/useShowOnboarding.ts
+++ cardstack/src/hooks/onboarding/useShowOnboarding.ts
@@ -6,6 +6,8 @@ import { remoteFlags } from '@cardstack/services/remote-config';
 export const useShowOnboarding = () => {
   const { primarySafe, isFetching, isLoading } = usePrimarySafe();
 
+  // Using a callback instead of memo to force pull the feature remote flag value on call.
+  // That is because it's a value inside react's side-effects and will not update a memo value.
   const shouldPresentOnboarding = useCallback(() => {
     return (
       !isLoading &&
