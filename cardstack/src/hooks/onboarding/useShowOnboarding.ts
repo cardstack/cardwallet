6a1173560a4ddc3f6f4fef6a5904ae14519306ba
Douglas Schmidt <github@oidoug.com>
2022-08-01 13:02:24 -0300

    Adds isLoading safe as dep for onboarding

--- cardstack/src/hooks/onboarding/useShowOnboarding.ts
+++ cardstack/src/hooks/onboarding/useShowOnboarding.ts
@@ -4,15 +4,16 @@ import { usePrimarySafe } from '@cardstack/redux/hooks/usePrimarySafe';
 import { remoteFlags } from '@cardstack/services/remote-config';
 
 export const useShowOnboarding = () => {
-  const { primarySafe, isFetching } = usePrimarySafe();
+  const { primarySafe, isFetching, isLoading } = usePrimarySafe();
 
   const shouldPresentOnboarding = useCallback(() => {
     return (
+      !isLoading &&
       !isFetching &&
       !primarySafe &&
       remoteFlags().featureProfilePurchaseOnboarding
     );
-  }, [isFetching, primarySafe]);
+  }, [isLoading, isFetching, primarySafe]);
 
   return {
     shouldPresentOnboarding,
