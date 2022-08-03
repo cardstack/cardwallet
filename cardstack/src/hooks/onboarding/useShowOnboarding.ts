33d184936f32b3588965294403c23578caafae51
Paula Penedo <paulinhapenedo@gmail.com>
2022-08-03 18:16:21 -0300

    Enhance showOnboarding hook to not show when importing an account (#976)

--- cardstack/src/hooks/onboarding/useShowOnboarding.ts
+++ cardstack/src/hooks/onboarding/useShowOnboarding.ts
@@ -4,19 +4,22 @@ import { usePrimarySafe } from '@cardstack/redux/hooks/usePrimarySafe';
 import { remoteFlags } from '@cardstack/services/remote-config';
 
 export const useShowOnboarding = () => {
-  const { primarySafe, isFetching, isLoading } = usePrimarySafe();
+  const {
+    primarySafe,
+    isFetching,
+    isLoading,
+    isUninitialized,
+  } = usePrimarySafe();
 
   // Using a callback instead of memo to pull the feature flag value on call.
   // That is because the flag is stateles and does not causes react's side-effects,
   // so it will not update the memo value.
   const shouldPresentOnboarding = useCallback(() => {
     return (
-      !isLoading &&
-      !isFetching &&
-      !primarySafe &&
+      !(isLoading && isFetching && primarySafe && isUninitialized) &&
       remoteFlags().featureProfilePurchaseOnboarding
     );
-  }, [isLoading, isFetching, primarySafe]);
+  }, [isLoading, isFetching, primarySafe, isUninitialized]);
 
   return {
     shouldPresentOnboarding,
