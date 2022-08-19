688b351d20fe8010b50323e570c882dd4c0d89a5
danibonilha <dani.bonilha@live.com>
2022-08-19 17:02:53 -0300

    [Refactor]: Remove dead components

--- src/components/header/index.js
+++
@@ -1,7 +0,0 @@
-export { default as BackButton } from './BackButton';
-export {
-  default as Header,
-  HeaderHeight,
-  HeaderHeightWithStatusBar,
-} from './Header';
-export { default as HeaderButton } from './HeaderButton';
