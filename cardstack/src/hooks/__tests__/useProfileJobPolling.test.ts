5b7129f36bf4de76d44ac0b32c1f89bcfbd0543d
Douglas Schmidt <douglaslondrina@gmail.com>
2022-08-22 15:27:05 -0300

    [CS-4455] Tests for polling hook (#1008)
    * Adds tests for checking different job states and retry mutation
    
    * Adds check for connection error return.
    
    * Update test to check fn param on call;
    Minor description changes.

---
+++ cardstack/src/hooks/__tests__/useProfileJobPolling.test.ts
@@ -0,0 +1,133 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { act } from '@testing-library/react-native';
+
+import {
+  useGetProfileJobStatusQuery,
+  usePostProfileJobRetryMutation,
+} from '@cardstack/services';
+
+import { useProfileJobPolling } from '../useProfileJobPolling';
+
+jest.mock('@cardstack/services', () => ({
+  useGetProfileJobStatusQuery: jest.fn(),
+  usePostProfileJobRetryMutation: jest.fn(),
+}));
+
+const mockJobParams = { jobID: 'ANY', onJobCompletedCallback: jest.fn() };
+
+const mockJobRetryMutation = jest.fn();
+
+describe('useProfileJobPolling', () => {
+  const mockUseGetProfileJobStatusQuery = (
+    state: 'pending' | 'success' | 'failed' = 'pending',
+    error?: any
+  ) => {
+    (useGetProfileJobStatusQuery as jest.Mock).mockImplementation(() => ({
+      data: {
+        attributes: {
+          state,
+        },
+      },
+      error,
+    }));
+  };
+
+  // Once isSuccess is true, polling restarts.
+  const mockUsePostProfileJobRetryMutation = (isSuccess = false) => {
+    (usePostProfileJobRetryMutation as jest.Mock).mockImplementation(() => [
+      mockJobRetryMutation,
+      { isSuccess },
+    ]);
+  };
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  beforeEach(() => {
+    mockUseGetProfileJobStatusQuery();
+    mockUsePostProfileJobRetryMutation();
+  });
+
+  it('should do nothing if jobID is not provided', () => {
+    const { result } = renderHook(() =>
+      useProfileJobPolling({ ...mockJobParams, jobID: undefined })
+    );
+
+    expect(result.current.isCreatingProfile).toBeFalsy();
+  });
+
+  it('should start hook polling when jobID provided', () => {
+    const { result } = renderHook(() => useProfileJobPolling(mockJobParams));
+
+    expect(result.current.isCreatingProfile).toBeTruthy();
+  });
+
+  it('should stop polling with no errors on profile success', () => {
+    const { result, rerender } = renderHook(() =>
+      useProfileJobPolling(mockJobParams)
+    );
+
+    mockUseGetProfileJobStatusQuery('success');
+    rerender();
+
+    expect(result.current.isCreatingProfile).toBeFalsy();
+  });
+
+  it('should return profile error flag once job has failed', () => {
+    const { result, rerender } = renderHook(() =>
+      useProfileJobPolling(mockJobParams)
+    );
+
+    mockUseGetProfileJobStatusQuery('failed');
+    rerender();
+
+    expect(result.current.isCreateProfileError).toBeTruthy();
+  });
+
+  it('should be able to call retry function if job has failed', () => {
+    const { result, rerender } = renderHook(() =>
+      useProfileJobPolling(mockJobParams)
+    );
+
+    mockUseGetProfileJobStatusQuery('failed');
+    rerender();
+
+    act(() => {
+      result.current.retryCurrentCreateProfile();
+    });
+
+    expect(mockJobRetryMutation).toBeCalledWith({
+      jobTicketID: mockJobParams.jobID,
+    });
+  });
+
+  it('should resume polling once retry call is successful', () => {
+    const { result, rerender } = renderHook(() =>
+      useProfileJobPolling(mockJobParams)
+    );
+
+    mockUseGetProfileJobStatusQuery('failed');
+    rerender();
+
+    expect(result.current.isCreatingProfile).toBeFalsy();
+
+    mockUsePostProfileJobRetryMutation(true);
+    rerender();
+
+    expect(result.current.isCreatingProfile).toBeTruthy();
+  });
+
+  it('should return isConnectionError on connection error', () => {
+    const { result, rerender } = renderHook(() =>
+      useProfileJobPolling(mockJobParams)
+    );
+
+    mockUseGetProfileJobStatusQuery(undefined, 'CON_ERROR');
+    rerender();
+
+    expect(result.current.isCreatingProfile).toBeFalsy();
+    expect(result.current.isCreateProfileError).toBeFalsy();
+    expect(result.current.isConnectionError).toMatch('CON_ERROR');
+  });
+});
