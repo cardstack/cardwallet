cf0163be4ca35dc84ee37c1bb996a013a7a234fc
Paula Penedo <paulinhapenedo@gmail.com>
2022-08-26 14:37:58 -0300

    feat: add longPress to QRCode on Pay screen

--- cardstack/src/screens/QRScannerScreen/pages/RequestQRCode/RequestQRCodePage.tsx
+++ cardstack/src/screens/QRScannerScreen/pages/RequestQRCode/RequestQRCodePage.tsx
@@ -1,5 +1,5 @@
-import React, { memo, useMemo } from 'react';
-import { StyleSheet } from 'react-native';
+import React, { memo, useCallback, useMemo } from 'react';
+import { Pressable, StyleSheet } from 'react-native';
 
 import {
   Button,
@@ -9,6 +9,7 @@ import {
   StyledQRCode,
   Text,
 } from '@cardstack/components';
+import { useCopyToast } from '@cardstack/hooks';
 
 import { ContactAvatar } from '@rainbow-me/components/contacts';
 import { useDimensions } from '@rainbow-me/hooks';
@@ -41,6 +42,15 @@ const RequestQRCodePage = () => {
     [isSmallPhone]
   );
 
+  const { CopyToastComponent, copyToClipboard } = useCopyToast({
+    dataToCopy: paymentRequestDeepLink,
+    customCopyLabel: 'QRCode',
+  });
+
+  const handleLongPressQRCode = useCallback(() => copyToClipboard(), [
+    copyToClipboard,
+  ]);
+
   if (!safeAddress) {
     // Temp placeholder for no merchant/profile
     return (
@@ -68,59 +78,64 @@ const RequestQRCodePage = () => {
   }
 
   return (
-    <CenteredContainer
-      paddingHorizontal={10}
-      flex={1}
-      backgroundColor="backgroundDarkPurple"
-      style={styles.container}
-    >
-      <Container flexShrink={1} justifyContent="flex-start">
-        <StyledQRCode value={paymentRequestDeepLink} />
-      </Container>
-      <Container flex={1} flexDirection="column">
-        <CenteredContainer flex={1}>
-          {merchantInfo?.name ? (
-            <ContactAvatar
-              color={merchantInfo?.color}
-              size={iconSize.avatar}
-              value={merchantInfo?.name}
-              textColor="white"
-            />
-          ) : (
-            <Icon name="user-with-background" size={iconSize.defaultIcon} />
-          )}
-          <Text
-            paddingTop={1}
-            fontWeight="bold"
-            ellipsizeMode="tail"
-            numberOfLines={1}
-            fontSize={20}
-            color="white"
-          >
-            {merchantInfo?.name || ''}
-          </Text>
-        </CenteredContainer>
-        <CenteredContainer flex={1}>
-          <Text
-            fontSize={18}
-            numberOfLines={1}
-            color="white"
-            textAlign="center"
-          >
-            {strings.requestViaText}
-          </Text>
-          <Button
-            variant="primaryWhite"
-            borderColor="teal"
-            maxWidth="95%"
-            marginTop={3}
-            onPress={onRequestAmountPress}
-          >
-            {strings.requestAmountBtn}
-          </Button>
-        </CenteredContainer>
-      </Container>
-    </CenteredContainer>
+    <>
+      <CenteredContainer
+        paddingHorizontal={10}
+        flex={1}
+        backgroundColor="backgroundDarkPurple"
+        style={styles.container}
+      >
+        <Container flexShrink={1} justifyContent="flex-start">
+          <Pressable onLongPress={handleLongPressQRCode}>
+            <StyledQRCode value={paymentRequestDeepLink} />
+          </Pressable>
+        </Container>
+        <Container flex={1} flexDirection="column">
+          <CenteredContainer flex={1}>
+            {merchantInfo?.name ? (
+              <ContactAvatar
+                color={merchantInfo?.color}
+                size={iconSize.avatar}
+                value={merchantInfo?.name}
+                textColor="white"
+              />
+            ) : (
+              <Icon name="user-with-background" size={iconSize.defaultIcon} />
+            )}
+            <Text
+              paddingTop={1}
+              fontWeight="bold"
+              ellipsizeMode="tail"
+              numberOfLines={1}
+              fontSize={20}
+              color="white"
+            >
+              {merchantInfo?.name || ''}
+            </Text>
+          </CenteredContainer>
+          <CenteredContainer flex={1}>
+            <Text
+              fontSize={18}
+              numberOfLines={1}
+              color="white"
+              textAlign="center"
+            >
+              {strings.requestViaText}
+            </Text>
+            <Button
+              variant="primaryWhite"
+              borderColor="teal"
+              maxWidth="95%"
+              marginTop={3}
+              onPress={onRequestAmountPress}
+            >
+              {strings.requestAmountBtn}
+            </Button>
+          </CenteredContainer>
+        </Container>
+      </CenteredContainer>
+      <CopyToastComponent />
+    </>
   );
 };
 
