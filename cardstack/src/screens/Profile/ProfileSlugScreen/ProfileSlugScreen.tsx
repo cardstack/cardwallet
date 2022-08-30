cbada2e5c7d5bf038cd82dff69590781e3388751
Paula Penedo <paulinhapenedo@gmail.com>
2022-08-30 17:16:22 -0300

    feat: replace InPageHeader w/ NavigationStackHeader in ProfileSlugScreen

--- cardstack/src/screens/Profile/ProfileSlugScreen/ProfileSlugScreen.tsx
+++ cardstack/src/screens/Profile/ProfileSlugScreen/ProfileSlugScreen.tsx
@@ -1,12 +1,12 @@
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import {
   Container,
   Text,
-  SafeAreaView,
   Button,
-  InPageHeader,
   ValidationMessage,
+  NavigationStackHeader,
   SuffixedInput,
 } from '@cardstack/components';
 
@@ -20,45 +20,62 @@ const ProfileSlugScreen = () => {
     slugValidation: { slugAvailable, detail: slugFeedback },
     canContinue,
     onContinuePress,
+    onSkipPress,
   } = useProfileSlugScreen();
 
-  return (
-    <SafeAreaView
-      backgroundColor="backgroundDarkPurple"
-      flex={1}
-      paddingHorizontal={5}
-      justifyContent="space-between"
-    >
-      <InPageHeader showLeftIcon={false} />
-      <Container flex={0.8}>
-        <Container width="90%" paddingBottom={4}>
-          <Text variant="pageHeader">{strings.header}</Text>
-        </Container>
+  // handles SafeAreaView bottom spacing for consistency
+  const { bottom } = useSafeAreaInsets();
 
-        <Container paddingBottom={1}>
-          <SuffixedInput
-            value={slug}
-            onChangeText={onSlugChange}
-            suffixText={strings.input.domainSuffix}
-          />
+  const containerStyles = useMemo(
+    () => ({
+      paddingBottom: bottom,
+    }),
+    [bottom]
+  );
+
+  return (
+    <Container
+      flex={1}
+      style={containerStyles}
+      backgroundColor="backgroundDarkPurple"
+    >
+      <NavigationStackHeader
+        canGoBack={false}
+        onSkipPress={onSkipPress}
+        backgroundColor="backgroundDarkPurple"
+        leftIconProps={{ iconSize: 'small' }}
+      />
+      <Container paddingHorizontal={5} flex={1} justifyContent="space-between">
+        <Container flex={0.8}>
+          <Container width="90%" paddingBottom={4}>
+            <Text variant="pageHeader">{strings.header}</Text>
+          </Container>
+
+          <Container paddingBottom={1}>
+            <SuffixedInput
+              value={slug}
+              onChangeText={onSlugChange}
+              suffixText={strings.input.domainSuffix}
+            />
+          </Container>
+          <Container width="100%">
+            <ValidationMessage
+              isVisible={!!slugFeedback}
+              isValid={slugAvailable}
+              message={slugFeedback}
+            />
+            <Text variant="pageDescriptionSmall">
+              {strings.input.description}
+            </Text>
+          </Container>
         </Container>
-        <Container width="100%">
-          <ValidationMessage
-            isVisible={!!slugFeedback}
-            isValid={slugAvailable}
-            message={slugFeedback}
-          />
-          <Text variant="pageDescriptionSmall">
-            {strings.input.description}
-          </Text>
+        <Container flex={0.2} justifyContent="flex-end" paddingBottom={5}>
+          <Button onPress={onContinuePress} blocked={!canContinue}>
+            {strings.buttons.continue}
+          </Button>
         </Container>
       </Container>
-      <Container flex={0.2} justifyContent="flex-end" paddingBottom={5}>
-        <Button onPress={onContinuePress} blocked={!canContinue}>
-          {strings.buttons.continue}
-        </Button>
-      </Container>
-    </SafeAreaView>
+    </Container>
   );
 };
 
