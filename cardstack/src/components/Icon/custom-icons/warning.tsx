3b2e37ee60c2fff652f24a6e44f61f160fe84380
Paula Penedo <paulinhapenedo@gmail.com>
2022-09-01 18:09:24 -0300

    fix: destruct props and assign values as default

--- cardstack/src/components/Icon/custom-icons/warning.tsx
+++ cardstack/src/components/Icon/custom-icons/warning.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import Svg, { SvgProps, Path, Circle } from 'react-native-svg';
 
-function SvgComponent(props: SvgProps) {
-  const baseFill = props.fill || '#ffa700';
-  const strokeColor = props.stroke || '#fff';
-
+function SvgComponent({ fill = '#ffa700', stroke = '#fff' }: SvgProps) {
   return (
     <Svg
       data-name="Icon_System Warning"
@@ -18,12 +15,12 @@ function SvgComponent(props: SvgProps) {
       <Path
         data-name="Path 455"
         d="M2.059 21.006L.383 17.852l9.8-16.723 1.838-1.09 1.789 1.09 9.809 16.723-1.549 3.154z"
-        fill={baseFill}
+        fill={fill}
       />
       <Path
         data-name="Path 382"
         d="M13.139 13.342h-2.343l-.49-6.924h3.327zm-2.871 3.288a1.61 1.61 0 01.432-1.22 1.752 1.752 0 011.258-.413 1.7 1.7 0 011.234.423 1.606 1.606 0 01.441 1.21 1.606 1.606 0 01-.442 1.2 1.672 1.672 0 01-1.229.437 1.722 1.722 0 01-1.252-.437 1.6 1.6 0 01-.442-1.2z"
-        fill={strokeColor}
+        fill={stroke} // using stroke as fill-color to enable two-color component
       />
       <Circle
         data-name="Ellipse 43"
@@ -31,7 +28,7 @@ function SvgComponent(props: SvgProps) {
         cy={2}
         r={2}
         transform="translate(10)"
-        fill={baseFill}
+        fill={fill}
       />
       <Circle
         data-name="Ellipse 44"
@@ -39,7 +36,7 @@ function SvgComponent(props: SvgProps) {
         cy={2}
         r={2}
         transform="translate(0 17)"
-        fill={baseFill}
+        fill={fill}
       />
       <Circle
         data-name="Ellipse 45"
@@ -47,7 +44,7 @@ function SvgComponent(props: SvgProps) {
         cy={2}
         r={2}
         transform="translate(20 17)"
-        fill={baseFill}
+        fill={fill}
       />
     </Svg>
   );
