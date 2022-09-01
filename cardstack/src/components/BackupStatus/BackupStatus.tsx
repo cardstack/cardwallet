cd1e1cd617089c6cf31b3385042a5867364712af
Paula Penedo <paulinhapenedo@gmail.com>
2022-09-01 18:05:49 -0300

    fix: change approach to BackupStatus  configuration

--- cardstack/src/components/BackupStatus/BackupStatus.tsx
+++ cardstack/src/components/BackupStatus/BackupStatus.tsx
@@ -11,35 +11,34 @@ interface ConfigProps {
   copy: string;
   color: ColorTypes;
   iconName: IconName;
+  stroke?: ColorTypes;
 }
 
-export const BackupStatus = memo(({ status }: BackupStatusProps) => {
-  const successConfig: ConfigProps = {
+const config: Record<BackupStatusProps['status'], ConfigProps> = {
+  success: {
     copy: 'Backed up',
     color: 'lightGreen',
     iconName: 'check-circle',
-  };
-
-  const missingConfig: ConfigProps = {
+    stroke: undefined,
+  },
+  missing: {
     copy: 'Not Backed Up',
     color: 'orange',
     iconName: 'warning',
-  };
+    stroke: 'black',
+  },
+};
 
-  const statusConfig = status === 'success' ? successConfig : missingConfig;
-  const hasStroke = status !== 'success' ? 'black' : undefined;
-
-  return (
-    <Container flexDirection="row" alignItems="center">
-      <Icon
-        name={statusConfig.iconName}
-        stroke={hasStroke}
-        size={20}
-        marginRight={3}
-      />
-      <Text variant="semibold" fontSize={13} color={statusConfig.color}>
-        {statusConfig.copy}
-      </Text>
-    </Container>
-  );
-});
+export const BackupStatus = memo(({ status }: BackupStatusProps) => (
+  <Container flexDirection="row" alignItems="flex-end">
+    <Icon
+      name={config[status].iconName}
+      stroke={config[status].stroke}
+      iconSize="medium"
+      marginRight={3}
+    />
+    <Text variant="semibold" fontSize={13} color={config[status].color}>
+      {config[status].copy}
+    </Text>
+  </Container>
+));
