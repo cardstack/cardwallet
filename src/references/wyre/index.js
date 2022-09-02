c3e02959b7b941c99959dc8e3e2d79f9b64f64ee
danibonilha <dani.bonilha@live.com>
2022-09-02 17:37:44 -0300

    [CS-4545]: Remove hardcoded supported countries

--- src/references/wyre/index.js
+++
@@ -1,7 +0,0 @@
-import { keys } from 'lodash';
-
-import { supportedCountries } from './supportedCountries';
-
-const WYRE_SUPPORTED_COUNTRIES_ISO = keys(supportedCountries);
-
-export { supportedCountries, WYRE_SUPPORTED_COUNTRIES_ISO };
