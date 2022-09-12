2a2e7020b97b480c2b831346db3a403131bafd19
Paula Penedo <paulinhapenedo@gmail.com>
2022-09-12 16:28:37 -0300

    chore: create screenGroup folder to handle screenGroup files

--- cardstack/src/navigation/index.ts
+++ cardstack/src/navigation/index.ts
@@ -2,6 +2,6 @@ export * from './hooks';
 export * from './routes';
 export * from './screens';
 export * from './presetOptions';
-export * from './profileScreenGroup';
+export * from './screenGroups';
 export { default as AppContainer } from './navigator';
 export { default as Navigation } from './Navigation';
