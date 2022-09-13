8d2559112fe7b7ad171d1393efb743df3282ef2b
Douglas Schmidt <github@oidoug.com>
2022-09-13 10:31:25 -0300

    Moves isReady to store, adds isInitializing flag while starting remote config.

--- cardstack/src/hooks/remote-configs/useRemoteConfigs.ts
+++ cardstack/src/hooks/remote-configs/useRemoteConfigs.ts
@@ -1,29 +1,24 @@
 import { useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { useBooleanState } from '@cardstack/hooks';
 import {
   initRemoteConfigsThunk,
   forceFetchRemoteConfigsThunk,
 } from '@cardstack/redux/remoteConfigSlice';
 
 import { AppState } from '@rainbow-me/redux/store';
-import logger from 'logger';
 
 export const useRemoteConfigs = () => {
   const dispatch = useDispatch();
-  const configs = useSelector((state: AppState) => state.remoteConfigSlice);
-  const [isReady, setIsReady] = useBooleanState();
 
-  const initRemoteConfigs = useCallback(async () => {
-    try {
-      dispatch(initRemoteConfigsThunk());
-    } catch (error) {
-      logger.sentry("Couldn't fetch remote config", error);
-    } finally {
-      setIsReady();
-    }
-  }, [setIsReady, dispatch]);
+  const { configs, isReady, isInitializing } = useSelector(
+    (state: AppState) => state.remoteConfigSlice
+  );
+
+  const initRemoteConfigs = useCallback(
+    () => dispatch(initRemoteConfigsThunk()),
+    [dispatch]
+  );
 
   const fetchRemoteConfigs = useCallback(
     () => dispatch(forceFetchRemoteConfigsThunk()),
@@ -31,8 +26,8 @@ export const useRemoteConfigs = () => {
   );
 
   useEffect(() => {
-    initRemoteConfigs();
-  }, [initRemoteConfigs]);
+    if (!isReady && !isInitializing) initRemoteConfigs();
+  }, [isReady, isInitializing, initRemoteConfigs]);
 
   return {
     isReady,
--- cardstack/src/redux/remoteConfigSlice.ts
+++ cardstack/src/redux/remoteConfigSlice.ts
@@ -7,6 +7,14 @@ import {
 } from '@cardstack/services/remote-config';
 import { remoteConfigDefaults } from '@cardstack/services/remote-config/remoteConfigDefaults';
 
+import logger from 'logger';
+
+interface RemoteConfigSliceType {
+  configs: RemoteConfigValues;
+  isInitializing: boolean;
+  isReady: boolean;
+}
+
 export const initRemoteConfigsThunk = createAsyncThunk(
   'remoteConfig/initRemoteConfigsThunk',
   loadRemoteConfigs
@@ -17,21 +25,50 @@ export const forceFetchRemoteConfigsThunk = createAsyncThunk(
   forceFetch
 );
 
-const initialState: RemoteConfigValues = remoteConfigDefaults;
+const initialState: RemoteConfigSliceType = {
+  configs: remoteConfigDefaults,
+  isInitializing: false,
+  isReady: false,
+};
 
 const remoteConfigSlice = createSlice({
   name: 'remoteConfigSlice',
   initialState,
   reducers: {},
   extraReducers: builder => {
+    builder.addCase(initRemoteConfigsThunk.pending, state => {
+      state.isInitializing = true;
+
+      return state;
+    });
+
     builder.addCase(
       initRemoteConfigsThunk.fulfilled,
-      (state, { payload }: PayloadAction<RemoteConfigValues>) => payload
+      (state, { payload }: PayloadAction<RemoteConfigValues>) => {
+        state.configs = payload;
+        state.isInitializing = false;
+        state.isReady = true;
+
+        return state;
+      }
     );
+
+    builder.addCase(initRemoteConfigsThunk.rejected, (state, { error }) => {
+      state.isInitializing = false;
+      state.isReady = true;
+
+      logger.sentry("Couldn't fetch remote config", error);
+
+      return state;
+    });
 
     builder.addCase(
       forceFetchRemoteConfigsThunk.fulfilled,
-      (state, { payload }: PayloadAction<RemoteConfigValues>) => payload
+      (state, { payload }: PayloadAction<RemoteConfigValues>) => {
+        state.configs = payload;
+
+        return state;
+      }
     );
   },
 });
