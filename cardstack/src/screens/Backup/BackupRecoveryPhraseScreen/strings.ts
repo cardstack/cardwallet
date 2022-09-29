8de45069481f78726f435c7ba9293975d558a895
Douglas Schmidt <github@oidoug.com>
2022-09-29 09:44:10 -0300

    Updated description to match of manual screen

--- cardstack/src/screens/Backup/BackupRecoveryPhraseScreen/strings.ts
+++ cardstack/src/screens/Backup/BackupRecoveryPhraseScreen/strings.ts
@@ -4,7 +4,7 @@ export const strings = {
   },
   success: { title: 'Backup Successful' },
   description:
-    'These 12 words are the keys to your wallet, please manually backup them. Do not share this with anyone.',
+    'These 12 words are the keys to your wallet. Write them down or copy them to a password manager. Do not share this with anyone.',
   disclaimer:
     '* Always be mindful of your surroundings when viewing your recovery phrase',
   cloudBackupBtn: (cloud: string) => `Back up to ${cloud}`,
