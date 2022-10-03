feb6fd9d2898cc1e1055cc33ca5978f0591acda9
Douglas Schmidt <github@oidoug.com>
2022-10-03 17:20:08 -0300

    Adds error alert if manual backup cant save. Rebased with develop.

--- cardstack/src/hooks/backup/useWalletManualBackup.js
+++ cardstack/src/hooks/backup/useWalletManualBackup.js
@@ -2,9 +2,16 @@ import { captureException } from '@sentry/react-native';
 import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 
+import { Alert } from '@rainbow-me/components/alerts';
 import { useWallets } from '@rainbow-me/hooks';
 import { setWalletManualBackup } from '@rainbow-me/redux/wallets';
 import logger from 'logger';
+
+const savingManualBackupError = {
+  title: 'Error while saving your wallet as manually backed up',
+  message:
+    'No information was lost, but your wallet will still appear as not backed-up. Please restart the app and try again',
+};
 
 export const useWalletManualBackup = () => {
   const dispatch = useDispatch();
@@ -15,8 +22,10 @@ export const useWalletManualBackup = () => {
       await dispatch(setWalletManualBackup(selectedWallet.id));
     } catch (e) {
       logger.sentry(
-        `Error setting manual backup for wallet ID: ${selectedWallet.id}`
+        `[BACKUP] Error setting manual backup for wallet ID: ${selectedWallet.id}`
       );
+
+      Alert(savingManualBackupError);
 
       captureException(e);
     }
