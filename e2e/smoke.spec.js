f1ddca739c4052def07bc98112cff2f6fcd8091a
Dani Bonilha <dani.bonilha@live.com>
2022-10-13 13:42:50 -0300

    [Cleanup]: Remove a couple more of old backup related functions (#1068)
    * [Cleanup]: Delete unsused function
    
    * [Cleanup]: Remove functions used only on old backup
    
    * [Cleanup]: Remove e2e tests from old flows

--- e2e/smoke.spec.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable jest/expect-expect */
-import * as Helpers from './helpers';
-
-describe('Smoke tests', () => {
-  it('Should show the welcome screen', async () => {
-    await Helpers.checkIfVisible('welcome-screen');
-  });
-});
--- src/components/alerts/DelayedAlert.js
+++
@@ -1,5 +0,0 @@
-import Alert from './Alert';
-
-export default function DelayedAlert(options, delayMs = 500) {
-  setTimeout(() => Alert(options), delayMs);
-}
