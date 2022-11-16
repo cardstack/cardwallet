7f76f17406e35235e804ab66b479f3533c0e88e8
Douglas Schmidt <github@oidoug.com>
2022-11-16 11:33:22 -0300

    Improves loading state for reward program banner

--- cardstack/src/screens/RewardsCenterScreen/components/RewardProgramHeader.tsx
+++ cardstack/src/screens/RewardsCenterScreen/components/RewardProgramHeader.tsx
@@ -2,17 +2,22 @@ import React, { memo } from 'react';
 
 import { CenteredContainer, Image, Text } from '@cardstack/components';
 
-import rewardBanner from '../../../assets/rewards-promo.png';
+import cardstackRewardBanner from '../../../assets/rewards-banner.png';
+import bannerBackground from '../../../assets/rewards-promo.png';
 
 export const RewardProgramHeader = memo(({ title }: { title?: string }) => (
   <CenteredContainer>
-    <Image source={rewardBanner} minWidth="100%" />
-    {!!title && (
-      <CenteredContainer position="absolute" width="60%" height="100%">
-        <Text color="white" variant="promoBannerTitle">
-          {title}
-        </Text>
-      </CenteredContainer>
+    {title ? (
+      <>
+        <Image source={bannerBackground} minWidth="100%" />
+        <CenteredContainer position="absolute" width="60%" height="100%">
+          <Text color="white" variant="promoBannerTitle">
+            {title}
+          </Text>
+        </CenteredContainer>
+      </>
+    ) : (
+      <Image source={cardstackRewardBanner} minWidth="100%" />
     )}
   </CenteredContainer>
 ));
