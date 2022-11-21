a7720e9e7b2609607d49ee446029725bba33739a
danibonilha <dani.bonilha@live.com>
2022-11-21 15:09:33 -0300

    [CS-4904]: Remove unused walletConnect components

--- src/components/walletconnect-list/index.js
+++
@@ -1,4 +0,0 @@
-export { default as WalletConnectExplainer } from './WalletConnectExplainer';
-export { default as WalletConnectExplainerItem } from './WalletConnectExplainerItem';
-export { default as WalletConnectList } from './WalletConnectList';
-export { default as WalletConnectListItem } from './WalletConnectListItem';
