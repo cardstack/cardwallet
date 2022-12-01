0752fa88d75de191dc248fc6e00b6f9c96c75a3a
danibonilha <dani.bonilha@live.com>
2022-12-01 14:47:34 -0300

    [CS-4932]: Refactor genericDisplay to show requesting chain

--- cardstack/src/components/TransactionConfirmationSheet/displays/GenericDisplay.tsx
+++ cardstack/src/components/TransactionConfirmationSheet/displays/GenericDisplay.tsx
@@ -2,30 +2,53 @@ import { getConstantByNetwork } from '@cardstack/cardpay-sdk';
 import React from 'react';
 
 import { useGetAssetBalance } from '@cardstack/hooks';
+import { NetworkType } from '@cardstack/types';
 
 import { ContactAvatar } from '@rainbow-me/components/contacts';
-import { useAccountProfile, useAccountSettings } from '@rainbow-me/hooks';
+import { useAccountProfile } from '@rainbow-me/hooks';
 
 import { Container, HorizontalDivider, NetworkBadge, Text } from '../..';
 import { TransactionConfirmationDisplayProps } from '../TransactionConfirmationSheet';
 
 import { SectionHeaderText } from './components/SectionHeaderText';
 
-export const GenericDisplay = (props: TransactionConfirmationDisplayProps) => {
-  return (
-    <>
-      <FromSection />
-      <HorizontalDivider />
-      <MessageSection {...props} />
-    </>
+export const GenericDisplay = ({
+  txNetwork,
+  messageRequest,
+}: TransactionConfirmationDisplayProps) => (
+  <>
+    <FromSection txNetwork={txNetwork} />
+    <HorizontalDivider />
+    <Container width="100%">
+      <SectionHeaderText>MESSAGE</SectionHeaderText>
+      <Container
+        paddingHorizontal={3}
+        paddingVertical={2}
+        marginTop={2}
+        borderRadius={10}
+        borderColor="borderGray"
+        borderWidth={1}
+      >
+        <Text variant="subText">{messageRequest}</Text>
+      </Container>
+    </Container>
+  </>
+);
+
+const FromSection = ({
+  txNetwork,
+}: Pick<TransactionConfirmationDisplayProps, 'txNetwork'>) => {
+  const {
+    accountColor,
+    accountName,
+    accountSymbol,
+    accountAddress,
+  } = useAccountProfile();
+
+  const nativeTokenSymbol = getConstantByNetwork(
+    'nativeTokenSymbol',
+    txNetwork
   );
-};
-
-const FromSection = () => {
-  const { accountColor, accountName, accountSymbol } = useAccountProfile();
-
-  const { network, accountAddress } = useAccountSettings();
-  const nativeTokenSymbol = getConstantByNetwork('nativeTokenSymbol', network);
 
   const balance = useGetAssetBalance({
     asset: {
@@ -35,7 +58,7 @@ const FromSection = () => {
       name: '',
     },
     accountAddress,
-    network,
+    network: txNetwork as NetworkType,
   });
 
   return (
@@ -50,7 +73,7 @@ const FromSection = () => {
           />
           <Container marginLeft={4}>
             <Text weight="extraBold">{accountName}</Text>
-            <NetworkBadge marginTop={2} />
+            <NetworkBadge marginTop={2} network={txNetwork} />
             <Container flexDirection="row" marginTop={2}>
               <Container backgroundColor="black" width={2} height="100%" />
               <Container marginLeft={4}>
@@ -64,26 +87,6 @@ const FromSection = () => {
             </Container>
           </Container>
         </Container>
-      </Container>
-    </Container>
-  );
-};
-
-const MessageSection = ({
-  messageRequest,
-}: TransactionConfirmationDisplayProps) => {
-  return (
-    <Container width="100%">
-      <SectionHeaderText>MESSAGE</SectionHeaderText>
-      <Container
-        paddingHorizontal={3}
-        paddingVertical={2}
-        marginTop={2}
-        borderRadius={10}
-        borderColor="borderGray"
-        borderWidth={1}
-      >
-        <Text variant="subText">{messageRequest}</Text>
       </Container>
     </Container>
   );
