e98183cf1f52959c195b2a04957080532b0314c5
danibonilha <dani.bonilha@live.com>
2022-12-13 16:30:16 -0300

    [CS-4987]: Improve addition to api on store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -8,6 +8,7 @@ import { biometryToggleSliceName } from '@cardstack/redux/biometryToggleSlice';
 import { persistedFlagsName } from '@cardstack/redux/persistedFlagsSlice';
 import { primarySafeSliceName } from '@cardstack/redux/primarySafeSlice';
 import { welcomeBannerSliceName } from '@cardstack/redux/welcomeBanner';
+import { coingeckoApi } from '@cardstack/services/eoa-assets/coingecko/coingecko-api';
 import { eoaAssetsApi } from '@cardstack/services/eoa-assets/eoa-assets-api';
 import { hubApi } from '@cardstack/services/hub/hub-api';
 import { safesApi } from '@cardstack/services/safes-api';
@@ -28,13 +29,36 @@ const persistConfig = {
   ],
 };
 
+const apis = {
+  safesApi,
+  hubApi,
+  wyreApi,
+  eoaAssetsApi,
+  serviceStatusApi,
+  coingeckoApi,
+};
+
+type APIPaths = keyof typeof apis;
+
+type APIReducers = {
+  [k in APIPaths]: typeof apis[k]['reducer'];
+};
+
+const apiValues = Object.values(apis);
+
+const mapApisToReducers = apiValues.reduce(
+  (reducers, api) => ({
+    ...reducers,
+    [api.reducerPath]: api.reducer,
+  }),
+  {} as APIReducers
+);
+
+const mapApisToMiddlewares = apiValues.map(api => api.middleware);
+
 const rootReducer = combineReducers({
   ...reducers,
-  [safesApi.reducerPath]: safesApi.reducer,
-  [hubApi.reducerPath]: hubApi.reducer,
-  [serviceStatusApi.reducerPath]: serviceStatusApi.reducer,
-  [eoaAssetsApi.reducerPath]: eoaAssetsApi.reducer,
-  [wyreApi.reducerPath]: wyreApi.reducer,
+  ...mapApisToReducers,
   [authSlice.name]: authSlice.reducer,
 });
 
@@ -51,15 +75,7 @@ const store = configureStore({
       serializableCheck: false, // we are currently storing some non-serializable objects in the store including wallet connect objects. it would be nice to fix this.
       immutableCheck: false, // without disabling this, we get a max call stack exceeded when switching from mainnet to xdai. It is likely due to storing an object in redux that has a circular reference to itself.
     });
-    middlewares.push(
-      ...[
-        safesApi.middleware,
-        serviceStatusApi.middleware,
-        hubApi.middleware,
-        wyreApi.middleware,
-        eoaAssetsApi.middleware,
-      ]
-    );
+    middlewares.push(...mapApisToMiddlewares);
 
     if (__DEV__) {
       if (enableReduxFlipper) {
