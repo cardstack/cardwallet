339a1ef7c87cfd631a314b0646120694f986df1e
danibonilha <dani.bonilha@live.com>
2022-12-21 16:28:30 -0300

    [Refactor]: Migrate used hooks to typescript

--- src/hooks/useHideSplashScreen.ts
+++ src/hooks/useHideSplashScreen.ts
@@ -1,17 +1,18 @@
 import { useCallback } from 'react';
 import { InteractionManager, StatusBar } from 'react-native';
 import RNBootSplash from 'react-native-bootsplash';
+import { Device } from '@cardstack/utils';
 
 export default function useHideSplashScreen() {
   return useCallback(() => {
     RNBootSplash.hide({ fade: true });
 
-    if (android) {
+    if (Device.isAndroid) {
       StatusBar.setBackgroundColor('transparent', false);
       StatusBar.setTranslucent(true);
     }
     // show the StatusBar
-    (ios && StatusBar.setHidden(false, 'fade')) ||
+    (Device.isIOS && StatusBar.setHidden(false, 'fade')) ||
       InteractionManager.runAfterInteractions(() => {
         StatusBar.setHidden(false, 'fade');
       });
