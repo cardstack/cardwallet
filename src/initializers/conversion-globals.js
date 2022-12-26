55a91bfabe279e75fd2fbdb6b76617b222208f50
danibonilha <dani.bonilha@live.com>
2022-12-26 12:54:48 -0300

    [CS-5049]: Fix deprecated buffer

--- src/initializers/conversion-globals.js
+++ src/initializers/conversion-globals.js
@@ -1,11 +1,11 @@
 if (typeof btoa === 'undefined') {
   global.btoa = function (str) {
-    return new Buffer(str, 'binary').toString('base64');
+    return Buffer.from(str).toString('base64');
   };
 }
 
 if (typeof atob === 'undefined') {
   global.atob = function (b64Encoded) {
-    return new Buffer(b64Encoded, 'base64').toString('binary');
+    return Buffer.from(b64Encoded, 'base64').toString('binary');
   };
 }
