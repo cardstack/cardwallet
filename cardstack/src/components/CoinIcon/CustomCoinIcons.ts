d561a41614e0c96c24921f324eb889a429fe31d5
Paula Penedo <paulinhapenedo@gmail.com>
2022-12-27 16:47:23 -0300

    [CS-5003] Add custom MATIC icon

--- cardstack/src/components/CoinIcon/CustomCoinIcons.ts
+++ cardstack/src/components/CoinIcon/CustomCoinIcons.ts
@@ -1,6 +1,7 @@
 import CARDCPXD_ICON from '../../assets/coins/card.cpxd.png';
 import CARD_ICON from '../../assets/coins/card.png';
 import DAICPXD_ICON from '../../assets/coins/dai.cpxd.png';
+import MATIC_ICON from '../../assets/coins/matic.png';
 import XDAI_ICON from '../../assets/coins/xdai.png';
 
 // coin icons list
@@ -20,5 +21,9 @@ export default [
   {
     symbol: 'XDAI',
     icon: XDAI_ICON,
+  },
+  {
+    symbol: 'MATIC',
+    icon: MATIC_ICON,
   },
 ];
