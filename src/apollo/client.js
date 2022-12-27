3e181647308361aa42c2f0581849ab234010196b
danibonilha <dani.bonilha@live.com>
2022-12-27 16:53:11 -0300

    [Cleanup]: Remove outdated apollo

--- src/apollo/client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
-
-export const compoundClient = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2',
-  }),
-});
