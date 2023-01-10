73475642cc2a3a1a70b07a2cc20d19bdfc78a942
Paula Penedo <paulinhapenedo@gmail.com>
2023-01-10 20:03:51 -0300

    [CS-5115] Rename Cardstack Wallet to Card Pay Wallet

--- src/components/settings-menu/strings.ts
+++ src/components/settings-menu/strings.ts
@@ -1,10 +1,12 @@
+import { appName } from '@cardstack/constants';
+
 export const strings = {
   errorMessage: 'Failed to get notification preferences',
   alert: {
     message: 'Please enable push notifications to be notified of transactions',
     dismissButton: 'Dismiss',
     askPermission: {
-      title: 'Cardstack Wallet would like to send you push notifications',
+      title: `${appName} would like to send you push notifications`,
       actionButton: 'Okay',
     },
     handleDeniedPermission: {
