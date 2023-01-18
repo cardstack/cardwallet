efae3c0b3a85880c48cc59629915072e123058da
Douglas Schmidt <github@oidoug.com>
2023-01-18 10:12:14 -0300

    Fixes branch name and tag cleaning on release script

--- ci-scripts/create-tag.js
+++ ci-scripts/create-tag.js
@@ -26,11 +26,16 @@ const createTag = async () => {
   const data = await response.json();
   const mostRecentTag = data[0].name;
   const [tagVersion, tagVersionNumber] = mostRecentTag.split('-');
-  const cleanTag = tagVersion.replace('v', '');
+  const cleanTag = tagVersion.split('-')[0].replace('v', '');
   const newTag = getNewTag(cleanTag);
   const updatedTagVersionNumber =
     cleanTag === newTag ? Number(tagVersionNumber) + 1 : 1;
-  const tagName = `v${newTag}-${updatedTagVersionNumber}`;
+  const tagName = `v${newTag}-${updatedTagVersionNumber}-release`;
+
+  const branchToTag =
+    process.env.VERSION_CHANGE_TYPE !== 'none'
+      ? process.env.VERSION_CHANGE_BRANCH_TO_TAG
+      : process.env.BRANCH_TO_TAG;
 
   await fetch(
     `${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_REPOSITORY}/releases`,
@@ -39,7 +44,7 @@ const createTag = async () => {
         owner: 'cardstack',
         repo: 'cardwallet',
         tag_name: tagName,
-        target_commitish: process.env.BRANCH_TO_TAG,
+        target_commitish: branchToTag,
         name: `${tagName}`,
         body: `Released at ${new Date(Date.now()).toISOString()}`,
       }),
