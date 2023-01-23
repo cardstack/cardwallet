ed420d3343320e1a74de6db1244e44c7dda74daf
Douglas Schmidt <douglaslondrina@gmail.com>
2023-01-23 11:12:00 -0300

    [CS-4967] Improved build actions (#1173)
    * [CS-4967] Adds set-tag on ios git actions
    
    * WIP Android, adds Fastlane plugin to get a version for tagging, updates action
    
    * Adds Gradle property VERSION_NAME
    
    * Adds new Fastlane plugin to get android version name
    
    * Simplifies tag workflow and improves DX
    
    * Restores ci-scripts
    
    * Renames Tag workflow to Create New Release
    
    * Renames tag.yml to release.yml
    
    * Enables pushing new tag to remote
    
    * Reverting renaming so the PR can be tested.
    
    * Recovering tag workflow changes
    
    * Fixes branch name and tag cleaning on release script
    
    * Restoring Fastlane changes
    
    * Fixes missing checkout on the job
    
    * Fix tag release name; Removes unused android lanes
    
    * push_to_git_remote does not push from a detached ref, trying out push_git_tags
    
    * Add missing target
    
    * Pushing tag from set tag lane

--- ci-scripts/create-tag.js
+++ ci-scripts/create-tag.js
@@ -30,7 +30,12 @@ const createTag = async () => {
   const newTag = getNewTag(cleanTag);
   const updatedTagVersionNumber =
     cleanTag === newTag ? Number(tagVersionNumber) + 1 : 1;
-  const tagName = `v${newTag}-${updatedTagVersionNumber}`;
+  const tagName = `v${newTag}-${updatedTagVersionNumber}-tagrelease`;
+
+  const branchToTag =
+    process.env.VERSION_CHANGE_TYPE !== 'none'
+      ? process.env.VERSION_CHANGE_BRANCH_TO_TAG
+      : process.env.BRANCH_TO_TAG;
 
   await fetch(
     `${process.env.GITHUB_API_URL}/repos/${process.env.GITHUB_REPOSITORY}/releases`,
@@ -39,7 +44,7 @@ const createTag = async () => {
         owner: 'cardstack',
         repo: 'cardwallet',
         tag_name: tagName,
-        target_commitish: process.env.BRANCH_TO_TAG,
+        target_commitish: branchToTag,
         name: `${tagName}`,
         body: `Released at ${new Date(Date.now()).toISOString()}`,
       }),
